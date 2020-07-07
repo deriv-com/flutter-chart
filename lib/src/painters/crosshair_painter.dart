d01927d51bcd09adb7129562e5775f6120eb0b81
Rustem Kakimov <ruskakimov@gmail.com>
2020-07-07 17:02:24 +0800

    Tick crosshair label

--- lib/src/painters/crosshair_painter.dart
+++ lib/src/painters/crosshair_painter.dart
@@ -1,3 +1,4 @@
+import 'package:deriv_chart/src/paint/paint_text.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -21,10 +22,10 @@ class CrosshairPainter extends CustomPainter {
     if (crosshairCandle == null) return;
 
     final crosshairPaint = Paint()..color = Colors.white;
-
     final x = epochToCanvasX(crosshairCandle.epoch);
+
     canvas.drawLine(
-      Offset(x, 0),
+      Offset(x, 40),
       Offset(x, size.height),
       crosshairPaint,
     );
@@ -35,8 +36,31 @@ class CrosshairPainter extends CustomPainter {
       crosshairPaint,
     );
 
+    paintText(
+      canvas,
+      text: crosshairCandle.close.toStringAsFixed(pipSize),
+      centerX: x,
+      centerY: 10,
+      style: TextStyle(
+        fontSize: 14,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
     final time = DateTime.fromMillisecondsSinceEpoch(crosshairCandle.epoch);
-    final timeLabel = DateFormat('Hms').format(time);
+    final timeLabel = DateFormat('dd MMM yy HH:mm:ss').format(time);
+
+    paintText(
+      canvas,
+      text: timeLabel,
+      centerX: x,
+      centerY: 28,
+      style: TextStyle(
+        fontSize: 12,
+        color: Colors.white70,
+      ),
+    );
   }
 
   @override
