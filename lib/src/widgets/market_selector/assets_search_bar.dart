b09c28f9e81e5979aefb16168e93fc4e1eae2ef6
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-07-13 14:03:39 +0430

    Switch to search mode and back

--- lib/src/widgets/market_selector/assets_search_bar.dart
+++ lib/src/widgets/market_selector/assets_search_bar.dart
@@ -10,34 +10,67 @@ class AssetsSearchBar extends StatefulWidget {
 }
 
 class _AssetsSearchBarState extends State<AssetsSearchBar> {
+  bool _isSearching = false;
+  FocusNode _searchFieldFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _searchFieldFocusNode = FocusNode();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: <Widget>[
         Align(
           alignment: Alignment.center,
-          child: TextFormField(
-            onChanged: (String text) => widget.onSearchTextChanged?.call(text),
-            cursorColor: Colors.white70,
-            textAlign: TextAlign.center,
-            decoration: new InputDecoration(
-              border: InputBorder.none,
-              focusedBorder: InputBorder.none,
-              enabledBorder: InputBorder.none,
-              errorBorder: InputBorder.none,
-              disabledBorder: InputBorder.none,
-              hintText: 'Asset',
-            ),
-          ),
+          child: _isSearching
+              ? TextFormField(
+                  focusNode: _searchFieldFocusNode,
+                  onChanged: (String text) =>
+                      widget.onSearchTextChanged?.call(text),
+                  cursorColor: Colors.white70,
+                  textAlign: TextAlign.center,
+                  decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: 'Search in assets'),
+                )
+              : InkWell(
+                  child: Text(
+                    'Assets',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  onTap: () => _setToSearchMode(),
+                ),
         ),
         Align(
           alignment: Alignment.centerRight,
-          child: IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
+          child: _isSearching
+              ? IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () => setState(() {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    _isSearching = false;
+                  }),
+                )
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () => _setToSearchMode(),
+                ),
         ),
       ],
     );
+  }
+
+  void _setToSearchMode() {
+    _searchFieldFocusNode.requestFocus();
+    setState(() => _isSearching = true);
   }
 }
