d44fd7543ceeb360d75f84cf19e90aa7d7e752cf
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-07-14 14:19:56 +0430

    Added symbols icons

--- lib/src/widgets/market_selector/asset_item.dart
+++ lib/src/widgets/market_selector/asset_item.dart
@@ -15,13 +15,21 @@ class AssetItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ListTile(
-        leading: Icon(Icons.money_off),
+        leading: Image(
+          width: 24,
+          height: 24,
+          image: AssetImage(
+            'assets/icons/${asset.name}.png',
+            package: 'deriv_chart',
+          ),
+        ),
         title: Text(
-          asset.displayName,
-          style: TextStyle(color: Color(0xFFC2C2C2)),
+          '${asset.displayName}',
+          style: TextStyle(fontSize: 14, color: Color(0xFFC2C2C2)),
         ),
         onTap: () => onAssetClicked?.call(asset, false),
         trailing: IconButton(
+          iconSize: 20,
           icon: Icon(
             asset.isFavorite ? Icons.star : Icons.star_border,
             color: const Color(0xFF6E6E6E),
