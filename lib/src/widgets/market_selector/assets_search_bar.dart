1bdf93239c9d47b4970f3ff203fcdc9efc8aff3f
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-07-30 14:24:26 +0430

    Cleanup SearchBar widget

--- lib/src/widgets/market_selector/assets_search_bar.dart
+++ lib/src/widgets/market_selector/assets_search_bar.dart
@@ -25,61 +25,68 @@ class _AssetsSearchBarState extends State<AssetsSearchBar> {
   }
 
   @override
-  Widget build(BuildContext context) => Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: _isSearching
-                ? TextFormField(
-                    controller: _textEditingController,
-                    focusNode: _searchFieldFocusNode,
-                    onChanged: (String text) =>
-                        widget.onSearchTextChanged?.call(text),
-                    cursorColor: Colors.white70,
-                    textAlign: TextAlign.center,
-                    decoration: new InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        hintText: 'Search assets'),
-                  )
-                : InkWell(
-                    child: Text(
-                      'Assets',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                    onTap: () => _switchToSearchMode(),
-                  ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: _isSearching
-                ? IconButton(
-                    icon: Icon(Icons.close, size: 20),
-                    onPressed: _textEditingController.value.text.isEmpty
-                        ? null
-                        : () {
-                            _textEditingController.clear();
-                            widget.onSearchTextChanged?.call('');
-                          },
-                  )
-                : IconButton(
-                    icon: Icon(Icons.search, size: 20),
-                    onPressed: () => _switchToSearchMode(),
-                  ),
-          ),
-          if (_isSearching)
+  Widget build(BuildContext context) => _isSearching
+      ? Row(
+          children: <Widget>[
+            _buildBackButton(),
+            Expanded(child: _buildTextField()),
+            _buildClearButton(),
+          ],
+        )
+      : Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            _buildHeaderText(),
             Align(
-              alignment: Alignment.centerLeft,
-              child: IconButton(
-                icon: Icon(Icons.arrow_back, size: 20),
-                onPressed: () => _switchToNormalMode(),
-              ),
+              alignment: Alignment.centerRight,
+              child: _buildSearchButton(),
             ),
-        ],
+          ],
+        );
+
+  Widget _buildHeaderText() => GestureDetector(
+        child: Text(
+          'Assets',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+        onTap: () => _switchToSearchMode(),
+      );
+
+  Widget _buildBackButton() => IconButton(
+        icon: Icon(Icons.arrow_back, size: 20),
+        onPressed: () => _switchToNormalMode(),
+      );
+
+  Widget _buildSearchButton() => IconButton(
+        icon: Icon(Icons.search, size: 20),
+        onPressed: () => _switchToSearchMode(),
+      );
+
+  Widget _buildClearButton() => IconButton(
+        icon: Icon(Icons.close, size: 20),
+        onPressed: _textEditingController.value.text.isEmpty
+            ? null
+            : () {
+                _textEditingController.clear();
+                widget.onSearchTextChanged?.call('');
+              },
+      );
+
+  Widget _buildTextField() => TextFormField(
+        controller: _textEditingController,
+        focusNode: _searchFieldFocusNode,
+        onChanged: (String text) => widget.onSearchTextChanged?.call(text),
+        cursorColor: Colors.white70,
+        textAlign: TextAlign.left,
+        decoration: new InputDecoration(
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+          hintText: 'Search assets',
+        ),
       );
 
   void _switchToNormalMode() {
