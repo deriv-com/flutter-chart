a3e592246f9b28b70abd202421750183b6ca8a9c
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-08-25 12:05:20 +0430

    Fix LightTheme base08 color

--- lib/src/theme/colors.dart
+++ lib/src/theme/colors.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-
 /// Deriv branding colors, these colors should not be changed. It can be called
 /// as [BrandColors.coral]
 class BrandColors {
@@ -36,7 +35,7 @@ class LightThemeColors {
   static const Color base05 = Color(0xFF3E3E3E);
   static const Color base06 = Color(0xFF323738);
   static const Color base07 = Color(0xFF151717);
-  static const Color base08 = Color(0xFFFFFFFF);
+  static const Color base08 = Color(0xFF0E0E0E);
   static const Color accentGreen = Color(0xFF00A79E);
   static const Color accentYellow = Color(0xFFFFAD3A);
   static const Color accentRed = Color(0xFFCC2E3D);
