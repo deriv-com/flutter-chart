c70294eaf611a6aea00a16ced2de7389d64b802e
Rustem Kakimov <ruskakimov@gmail.com>
2020-09-08 11:27:38 +0800

    Doc new line

--- lib/src/callbacks.dart
+++ lib/src/callbacks.dart
@@ -1,4 +1,5 @@
 /// Callback when user scrolls back and gap with no data is scrolled into view.
+///
 /// [count] is the number of candles that should be appended to the front of the
 /// candles list.
 typedef OnLoadHistory = Function(int count);
