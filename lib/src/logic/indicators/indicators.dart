59c05265ed2a09caa9d799151b5986b1078a4af9
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-09-09 11:46:13 +0430

    Minor refactor in Indicators class

--- lib/src/logic/indicators/indicators.dart
+++ lib/src/logic/indicators/indicators.dart
@@ -1,85 +1,57 @@
 import 'package:deriv_chart/src/models/tick.dart';
 
-/// A class to calculate Moving Average, RSI, MACD indicators
+/// A class to calculate Moving Average
+// TODO(ramin): It's performance might not be ideal. Its only for the purpose of showing how to add multiple data series to the chart.
 class MovingAverage {
-  final int length;
-  int circIndex = -1;
-  bool filled = false;
-  double current = double.nan;
-  double oneOverLength;
-  List<Tick> circularBuffer;
-  double total = 0;
+  final int _period;
+  int _index = -1;
+  bool _filled = false;
+  double _current = double.nan;
+  double _oneOverLength;
+  List<Tick> _circularBuffer;
+  double _total = 0;
 
-  MovingAverage(this.length) {
-    this.oneOverLength = 1.0 / length;
-    this.circularBuffer = new List<Tick>(length);
-  }
-
-  MovingAverage update(Tick entry) {
-    Tick lostValue = circularBuffer[circIndex];
-    circularBuffer[circIndex] = entry;
-
-    // Maintain totals for Push function
-    total += entry.quote;
-    total -= lostValue.quote;
-
-    // If not yet filled, just return. Current value should be double.NaN
-    if (!filled) {
-      current = double.nan;
-      return this;
-    }
-
-    // Compute the average
-    double average = 0.0;
-    for (Tick aCircularBuffer in circularBuffer) {
-      average += aCircularBuffer.quote;
-    }
-
-    current = average * oneOverLength;
-
-    return this;
+  MovingAverage(this._period) {
+    this._oneOverLength = 1.0 / _period;
+    this._circularBuffer = List<Tick>(_period);
   }
 
   MovingAverage push(Tick entry) {
     // Apply the circular buffer
-    if (++circIndex == length) {
-      circIndex = 0;
+    if (++_index == _period) {
+      _index = 0;
     }
 
-    double lostValue = circularBuffer[circIndex]?.quote ?? 0;
-    circularBuffer[circIndex] = entry;
+    double lostValue = _circularBuffer[_index]?.quote ?? 0;
+    _circularBuffer[_index] = entry;
 
     // Compute the average
-    total += entry.quote;
-    total -= lostValue;
+    _total += entry.quote;
+    _total -= lostValue;
 
     // If not yet filled, just return. Current value should be double.NaN
-    if (!filled && circIndex != length - 1) {
-      current = double.nan;
+    if (!_filled && _index != _period - 1) {
+      _current = double.nan;
       return this;
     } else {
       // Set a flag to indicate this is the first time the buffer has been filled
-      filled = true;
+      _filled = true;
     }
 
-    current = total * oneOverLength;
+    _current = _total * _oneOverLength;
 
     return this;
   }
 
-  double getCurrent() {
-    return current;
-  }
-
   static List<Tick> movingAverage(List<Tick> input, int period) {
-    final MovingAverage ma = new MovingAverage(period);
+    final MovingAverage ma = MovingAverage(period);
 
-    final List<Tick> output = List<Tick>(/*input.length*/);
+    final List<Tick> output = List<Tick>();
 
     for (int i = 0; i < input.length; i++) {
       ma.push(input[i]);
-      if (!ma.getCurrent().isNaN) {
-        output.add(Tick(epoch: input[i].epoch, quote: ma.getCurrent()));
+      if (!ma._current.isNaN) {
+        output.add(Tick(epoch: input[i].epoch, quote: ma._current));
       }
     }
 
