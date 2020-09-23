319fc0613df9ec6bde200ccea819db82d66b7da0
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-09-23 16:29:55 +0330

    Fix lint warnings

--- lib/src/paint/paint_loading.dart
+++ lib/src/paint/paint_loading.dart
@@ -10,26 +10,26 @@ void paintLoadingAnimation({
   @required double loadingAnimationProgress,
   @required double loadingRightBoundX,
 }) {
-  final loadingPaint = Paint()
+  final Paint loadingPaint = Paint()
     ..color = Colors.white12
     ..strokeWidth = 1
     ..style = PaintingStyle.fill;
 
-  final numberOfBars = 32;
-  final rectWidth = max(size.width, size.height);
-  final barWidthAndSpace = rectWidth / numberOfBars;
-  final barWidth = barWidthAndSpace / 2;
+  const int numberOfBars = 32;
+  final double rectWidth = max(size.width, size.height);
+  final double barWidthAndSpace = rectWidth / numberOfBars;
+  final double barWidth = barWidthAndSpace / 2;
 
   double convertToLoadingRange(double x) =>
       x - (rectWidth - loadingRightBoundX);
 
-  final topLeftPath = Path();
-  final bottomRightPath = Path();
+  final Path topLeftPath = Path();
+  final Path bottomRightPath = Path();
 
   double barX = 0;
 
   for (int i = 0; i < numberOfBars; i++) {
-    final barPosition = convertToLoadingRange(
+    final double barPosition = convertToLoadingRange(
       (barX + (loadingAnimationProgress * rectWidth)) % rectWidth,
     );
 
@@ -39,46 +39,53 @@ void paintLoadingAnimation({
     double startPointX, startPointY;
 
     if (barPosition >= 0) {
-      // A bar in top-left triangle
-      startPointX = 0;
-      startPointY = barPosition;
-
       if (barPosition > size.height) {
         startPointX = barPosition - size.height;
         startPointY = size.height;
+      } else {
+        startPointX = 0;
+        startPointY = barPosition;
       }
 
-      topLeftPath.moveTo(startPointX, startPointY);
-      topLeftPath.lineTo(barPosition, 0);
+      // A bar in top-left triangle
+      topLeftPath
+        ..moveTo(startPointX, startPointY)
+        ..lineTo(barPosition, 0);
+
       if (barPosition + barWidth < loadingRightBoundX) {
         topLeftPath.lineTo(barPosition + barWidth, 0);
       } else {
-        final barEndJutOut = barPosition + barWidth - loadingRightBoundX;
-        topLeftPath.lineTo(barPosition + barWidth - barEndJutOut, 0);
-        topLeftPath.lineTo(barPosition + barWidth - barEndJutOut, barEndJutOut);
+        final double barEndJutOut = barPosition + barWidth - loadingRightBoundX;
+        topLeftPath
+          ..lineTo(barPosition + barWidth - barEndJutOut, 0)
+          ..lineTo(barPosition + barWidth - barEndJutOut, barEndJutOut);
       }
       topLeftPath.lineTo(startPointX, startPointY + barWidth);
 
       canvas.drawPath(topLeftPath, loadingPaint);
     }
 
-    startPointX = 0;
     startPointY = rectWidth + barPosition;
 
     if (startPointY > size.height) {
       startPointX = (loadingRightBoundX * (size.height - startPointY)) /
           (rectWidth - loadingRightBoundX + barPosition - startPointY);
       startPointY = size.height;
+    } else {
+      startPointX = 0;
     }
 
     if (startPointX <= loadingRightBoundX) {
       // A bar in bottom-right triangle
-      bottomRightPath.moveTo(startPointX, startPointY);
-      bottomRightPath.lineTo(
-          loadingRightBoundX, rectWidth - (loadingRightBoundX - barPosition));
-      bottomRightPath.lineTo(loadingRightBoundX,
-          rectWidth - (loadingRightBoundX - barPosition) + barWidth);
-      bottomRightPath.lineTo(startPointX, startPointY + barWidth);
+      bottomRightPath
+        ..moveTo(startPointX, startPointY)
+        ..lineTo(
+          loadingRightBoundX,
+          rectWidth - (loadingRightBoundX - barPosition),
+        )
+        ..lineTo(loadingRightBoundX,
+            rectWidth - (loadingRightBoundX - barPosition) + barWidth)
+        ..lineTo(startPointX, startPointY + barWidth);
 
       canvas.drawPath(bottomRightPath, loadingPaint);
     }
