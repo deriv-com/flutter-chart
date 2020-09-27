1bcb81e94e1968834588711b550ebe38f2c56557
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-09-27 08:23:23 +0330

    Docs for trading_times.dart

--- example/lib/utils/trading_times.dart
+++ example/lib/utils/trading_times.dart
@@ -7,6 +7,7 @@ import 'package:flutter_deriv_api/api/common/models/symbol_model.dart';
 import 'package:flutter_deriv_api/api/common/trading/trading_times.dart';
 import 'package:intl/intl.dart';
 
+/// Markets status change callback. (List of symbols that have been changed.)
 typedef OnMarketsStatusChange = void Function(List<SymbolStatusChange> symbols);
 
 /// A class to to remind us when there is change on opening and closing a market.
@@ -35,6 +36,10 @@ class TradingTimesReminder {
   /// open/closed status is changed.
   final OnMarketsStatusChange onMarketsStatusChange;
 
+  /// List of upcoming market change times.
+  ///
+  /// [SplayTreeMap] has been used to have the change times in correct order
+  /// while we are adding new entries to it.
   final SplayTreeMap<DateTime, List<SymbolStatusChange>> statusChangeTimes =
       SplayTreeMap<DateTime, List<SymbolStatusChange>>(
           (DateTime d1, DateTime d2) => d1.compareTo(d2));
@@ -94,6 +99,8 @@ class TradingTimesReminder {
     ));
   }
 
+  /// Removes the next upcoming market change time from [statusChangeTimes] and
+  /// sets a timer for it.
   Future<void> _setReminderTimer() async {
     _reminderTimer?.cancel();
 
@@ -117,7 +124,7 @@ class TradingTimesReminder {
 }
 
 /// Model class containing symbols status change information,
-/// whether the [symbol] goes open/close at a specific time.
+/// whether the [symbol] opens/closes at a specific time.
 class SymbolStatusChange {
   /// Initializes
   SymbolStatusChange(this.symbol, {this.goesOpen});
