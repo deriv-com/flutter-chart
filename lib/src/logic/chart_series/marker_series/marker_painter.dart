0d82a12803483f0a4ea0c7ef9c7d7dc1619ffb15
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-13 14:52:16 +0800

    Draw arrow

--- lib/src/logic/chart_series/marker_series/marker_painter.dart
+++ lib/src/logic/chart_series/marker_series/marker_painter.dart
@@ -64,5 +64,35 @@ class MarkerPainter extends DataPainter<MarkerSeries> {
         1.5,
         Paint()..color = Colors.black,
       );
+    _drawArrow(canvas, center, Size(12, 12));
+  }
+
+  void _drawArrow(Canvas canvas, Offset center, Size size) {
+    final Path path = Path();
+    canvas
+      ..save()
+      ..translate(center.dx - size.width / 2, center.dy - size.height / 2)
+      ..scale(size.width / 16, size.height / 16); // 16x16 is original svg size
+    // This path was generated with http://demo.qunee.com/svg2canvas/.
+    path
+      ..moveTo(9.41, 1.70999)
+      ..cubicTo(9.22425, 1.52403, 9.00368, 1.37652, 8.76088, 1.27587)
+      ..cubicTo(8.51808, 1.17522, 8.25783, 1.12341, 7.995, 1.12341)
+      ..cubicTo(7.73217, 1.12341, 7.47192, 1.17522, 7.22912, 1.27587)
+      ..cubicTo(6.98632, 1.37652, 6.76575, 1.52403, 6.58, 1.70999)
+      ..lineTo(3, 5.24999)
+      ..cubicTo(3.3743, 5.61946, 3.87907, 5.82662, 4.405, 5.82662)
+      ..cubicTo(4.93093, 5.82662, 5.4357, 5.61946, 5.81, 5.24999)
+      ..lineTo(7, 4.12999)
+      ..lineTo(7, 15)
+      ..lineTo(9, 14)
+      ..lineTo(9, 4.12999)
+      ..lineTo(10.13, 5.25999)
+      ..cubicTo(10.5047, 5.63249, 11.0116, 5.84157, 11.54, 5.84157)
+      ..cubicTo(12.0684, 5.84157, 12.5753, 5.63249, 12.95, 5.25999)
+      ..lineTo(9.41, 1.70999);
+    canvas
+      ..drawPath(path, Paint()..color = Colors.white)
+      ..restore();
   }
 }
