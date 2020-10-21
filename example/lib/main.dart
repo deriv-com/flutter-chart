966caf4c49c82bc4c19baf0c6d6874a8d2f6e36f
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-10-21 13:04:12 +0330

    Update opacity param used in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -368,7 +368,7 @@ class _FullscreenChartState extends State<FullscreenChart> {
                     controller: _controller,
                     isLive: (_symbol?.isOpen ?? false) &&
                         (_connectionBloc?.state is Connected ?? false),
-                    enabledOpacity: _symbol?.isOpen ?? true ? 1.0 : 0.5,
+                    opacity: _symbol?.isOpen ?? true ? 1.0 : 0.5,
                     onCrosshairAppeared: () => Vibration.vibrate(duration: 50),
                     onVisibleAreaChanged: (int leftEpoch, int rightEpoch) {
                       if (!_waitingForHistory &&
