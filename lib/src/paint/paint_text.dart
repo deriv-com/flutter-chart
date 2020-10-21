ac4a3263235b4782421973ebac12485ae0253f0a
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-21 16:28:57 +0800

    Split paint text

--- lib/src/paint/paint_text.dart
+++ lib/src/paint/paint_text.dart
@@ -8,21 +8,56 @@ void paintText(
   Alignment anchorAlignment = Alignment.center,
   TextStyle style,
 }) {
+  final TextPainter painter = makeTextPainter(text, style);
+  paintWithTextPainter(
+    canvas,
+    painter: painter,
+    anchor: anchor,
+    anchorAlignment: anchorAlignment,
+  );
+}
+
+/// Constructs a text painter and performs layout.
+///
+/// Use this in combination with `paintWithTextPainter`,
+/// if you need to know text size on the canvas.
+///
+/// e.g.
+/// ```
+/// final tp = makeTextPainter('Hello', style);
+/// final w = tp.width; // text width
+/// final h = tp.height; // text height
+///
+/// paintWithTextPainter(
+///   canvas,
+///   painter: tp,
+///   anchor: Offset(0, 0),
+///   anchorAlignment: Alignment.centerRight,
+/// );
+/// ```
+TextPainter makeTextPainter(String text, TextStyle style) {
   final TextSpan span = TextSpan(
     text: text,
     style: style,
   );
-  final TextPainter tp = TextPainter(
+  return TextPainter(
     text: span,
     textDirection: TextDirection.ltr,
+  )..layout();
+}
+
+/// Paints on the canvas with the given text painter.
+void paintWithTextPainter(
+  Canvas canvas, {
+  @required TextPainter painter,
+  @required Offset anchor,
+  Alignment anchorAlignment = Alignment.center,
+}) {
+  painter.paint(
+    canvas,
+    Offset(
+      anchor.dx - painter.width / 2 * (anchorAlignment.x + 1),
+      anchor.dy - painter.height / 2 * (anchorAlignment.y + 1),
+    ),
   );
-  tp
-    ..layout()
-    ..paint(
-      canvas,
-      Offset(
-        anchor.dx - tp.width / 2 * (anchorAlignment.x + 1),
-        anchor.dy - tp.height / 2 * (anchorAlignment.y + 1),
-      ),
-    );
 }
