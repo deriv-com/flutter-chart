545c2529267d23da6fe206f1da69e5bd2d42c1fa
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-10 13:58:19 +0800

    Add docs

--- lib/src/x_axis/grid/x_grid_painter.dart
+++ lib/src/x_axis/grid/x_grid_painter.dart
@@ -4,15 +4,22 @@ import 'package:flutter/material.dart';
 import 'paint_x_grid.dart';
 import 'time_label.dart';
 
+/// Paints x-axis grid and labels.
 class XGridPainter extends CustomPainter {
+  /// Creates x-axis painter.
   XGridPainter({
     @required this.gridTimestamps,
     @required this.epochToCanvasX,
     @required this.style,
   });
 
+  /// Timestamps of vertical grid lines.
   final List<DateTime> gridTimestamps;
+
+  /// Epoch to x-coordinate convertion function.
   final double Function(int) epochToCanvasX;
+
+  /// Style of the grid.
   final GridStyle style;
 
   @override
