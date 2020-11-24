dc3799413bfacf675056216b905e371cbf18a9a2
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-24 16:49:36 +0800

    Binary search overlapping gap range

--- lib/src/x_axis/gaps/duration_without_gaps.dart
+++ lib/src/x_axis/gaps/duration_without_gaps.dart
@@ -1,13 +1,36 @@
 import 'package:deriv_chart/src/models/time_range.dart';
 
+int _indexOfGapThatContainsOrNearEpoch(List<TimeRange> gaps, int epoch) {
+  int low = 0, high = gaps.length - 1;
+
+  while (low < high) {
+    final int mid = (low + high) >> 1;
+    if (gaps[mid].isBefore(epoch)) {
+      high = mid;
+    } else if (gaps[mid].isAfter(epoch)) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return low;
+}
+
 /// [range] duration with [gaps] removed.
 int durationWithoutGaps(
   TimeRange range,
   List<TimeRange> gaps,
 ) {
+  final int left = _indexOfGapThatContainsOrNearEpoch(gaps, range.leftEpoch);
+  final int right = _indexOfGapThatContainsOrNearEpoch(gaps, range.rightEpoch);
+
   int overlap = 0;
-  for (final TimeRange gap in gaps) {
-    overlap += gap.overlap(range)?.duration ?? 0;
+
+  overlap += gaps[left].overlap(range)?.duration ?? 0;
+  overlap += gaps[right].overlap(range)?.duration ?? 0;
+
+  for (int i = left + 1; i < right; i++) {
+    overlap += gaps[i].duration;
   }
   return range.duration - overlap;
 }
