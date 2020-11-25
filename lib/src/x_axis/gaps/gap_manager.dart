80550566f3a3c86828200b769e65ccd801b7b17f
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-25 17:11:33 +0800

    Add cumulative sums docs

--- lib/src/x_axis/gaps/gap_manager.dart
+++ lib/src/x_axis/gaps/gap_manager.dart
@@ -5,6 +5,18 @@ import 'package:deriv_chart/src/x_axis/gaps/duration_without_gaps.dart';
 class GapManager {
   List<TimeRange> gaps = [];
 
+  /// Cumulative sums of gap durations from right to left.
+  /// Allows getting a sum of any gap range in constant time.
+  ///
+  /// Right to left is chosed to avoid recalculations,
+  /// since new gaps are added on the left.
+  ///
+  /// Example:
+  /// 10-20 30-40 60-80 - [gaps]
+  /// 10    10    20    - gap durations
+  /// 40    30    20    - [_cumulativeSums]
+  List<int> _cumulativeSums;
+
   void replaceGaps(List<TimeRange> newGaps) {
     gaps = newGaps;
   }
