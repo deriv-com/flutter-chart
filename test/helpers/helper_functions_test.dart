958c44e518cc16aab4a85b414662de566db452f1
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-11-25 11:18:51 +0330

    A minor refactor in helper_functions_test

--- test/helpers/helper_functions_test.dart
+++ test/helpers/helper_functions_test.dart
@@ -4,14 +4,14 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('roundDouble function', () {
     test('Rounds to the ceiling', () {
-      final double value = 2.6666883;
+      const double value = 2.6666883;
 
       expect(roundDouble(value, 4), 2.6667);
       expect(roundDouble(value, 3), 2.667);
     });
 
     test('Rounds to the floor', () {
-      final double value = 2.6666233;
+      const double value = 2.6666233;
 
       expect(roundDouble(value, 4), 2.6666);
     });
