826872e21de6f3de6de7e32c26e2d01eaed8f6ee
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-11-29 14:22:11 +0330

    Adding indicators from IndicatorsDialog

--- lib/src/chart_package.dart
+++ lib/src/chart_package.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:deriv_chart/deriv_chart.dart';
 import 'package:deriv_chart/src/chart.dart';
 import 'package:deriv_chart/src/logic/annotations/chart_annotation.dart';
 import 'package:deriv_chart/src/chart_controller.dart';
@@ -75,7 +76,8 @@ class ChartPackage extends StatefulWidget {
 }
 
 class _ChartPackageState extends State<ChartPackage> {
-  final List<Series> _indicators = <Series>[];
+  Map<String, IndicatorSeriesBuilder> _indicators =
+      <String, IndicatorSeriesBuilder>{};
 
   @override
   Widget build(BuildContext context) => Stack(
@@ -85,7 +87,13 @@ class _ChartPackageState extends State<ChartPackage> {
             pipSize: widget.pipSize,
             granularity: widget.granularity,
             controller: widget.controller,
-            secondarySeries: _indicators,
+            secondarySeries: <Series>[
+              ..._indicators.values
+                  .where((IndicatorSeriesBuilder indicatorBuilder) =>
+                      indicatorBuilder != null)
+                  .map((IndicatorSeriesBuilder indicatorBuilder) =>
+                      indicatorBuilder?.call(widget.mainSeries.entries))
+            ],
             markerSeries: widget.markerSeries,
             theme: widget.theme,
             onCrosshairAppeared: widget.onCrosshairAppeared,
@@ -103,7 +111,13 @@ class _ChartPackageState extends State<ChartPackage> {
                   builder: (
                     BuildContext context,
                   ) =>
-                      _IndicatorsDialog(indicators: _indicators),
+                      _IndicatorsDialog(
+                    ticks: widget.mainSeries.entries,
+                    onIndicators: (
+                      Map<String, IndicatorSeriesBuilder> indicators,
+                    ) =>
+                        setState(() => _indicators = indicators),
+                  ),
                 );
               },
             ),
@@ -113,41 +127,90 @@ class _ChartPackageState extends State<ChartPackage> {
 }
 
 class _IndicatorsDialog extends StatefulWidget {
-  const _IndicatorsDialog({Key key, this.indicators}) : super(key: key);
+  const _IndicatorsDialog({Key key, this.onIndicators, this.ticks})
+      : super(key: key);
 
-  final List<Series> indicators;
+  final List<Tick> ticks;
+  final OnIndicators onIndicators;
 
   @override
   _IndicatorsDialogState createState() => _IndicatorsDialogState();
 }
 
 class _IndicatorsDialogState extends State<_IndicatorsDialog> {
+  static final Map<String, GlobalKey<_IndicatorItemState>> keys =
+      <String, GlobalKey<_IndicatorItemState>>{
+    'MA': GlobalKey<MAIndicatorSeriesState>(),
+  };
+
+  final Map<String, IndicatorSeriesBuilder> indicatorsMap =
+      <String, IndicatorSeriesBuilder>{};
+
+  final List<IndicatorItem<Series>> indicatorItems = <IndicatorItem>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    indicatorItems
+      ..add(MAIndicatorItem(
+        key: keys['MA'],
+        ticks: widget.ticks,
+        onAddIndicator: _onAddIndicator,
+      ));
+  }
+
+  void _onAddIndicator(
+    String key,
+    IndicatorSeriesBuilder indicatorBuilder,
+  ) =>
+      setState(() {
+        indicatorsMap[key] = indicatorBuilder;
+        widget.onIndicators(indicatorsMap);
+      });
+
   @override
   Widget build(BuildContext context) => AnimatedPopupDialog(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: const <Widget>[
-        IndicatorItem(
-          title: 'Moving Average',
-        )
-      ],
-    ),
-  );
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: indicatorItems.length,
+          itemBuilder: (BuildContext context, int index) =>
+              indicatorItems[index],
+        ),
+      );
 }
 
+typedef IndicatorSeriesBuilder = Series Function(List<Tick> ticks);
+typedef OnIndicators = Function(Map<String, IndicatorSeriesBuilder> indicators);
+typedef OnAddIndicator = Function(
+  String key,
+  IndicatorSeriesBuilder indicatorBuilder,
+);
+
 /// Indicator item in indicators dialog
-class IndicatorItem extends StatefulWidget {
+abstract class IndicatorItem<T extends Series> extends StatefulWidget {
   /// Initializes
-  const IndicatorItem({Key key, this.title}) : super(key: key);
+  const IndicatorItem({
+    Key key,
+    this.title,
+    this.ticks,
+    this.onAddIndicator,
+  }) : super(key: key);
 
   /// Title
   final String title;
 
+  final List<Tick> ticks;
+
+  final OnAddIndicator onAddIndicator;
+
   @override
-  _IndicatorItemState createState() => _IndicatorItemState();
+  _IndicatorItemState createState() => createIndicatorItemState();
+
+  _IndicatorItemState createIndicatorItemState();
 }
 
-class _IndicatorItemState extends State<IndicatorItem> {
+abstract class _IndicatorItemState extends State<IndicatorItem> {
   bool _indicatorIsActive = false;
 
   @override
@@ -156,8 +219,46 @@ class _IndicatorItemState extends State<IndicatorItem> {
         trailing: Checkbox(
           value: _indicatorIsActive,
           onChanged: (bool newValue) => setState(
-            () => _indicatorIsActive = newValue,
+            () {
+              _indicatorIsActive = newValue;
+
+              if (newValue) {
+                widget.onAddIndicator?.call(
+                  _getIndicatorKey(),
+                  createIndicatorSeries(),
+                );
+              } else {
+                widget.onAddIndicator?.call(_getIndicatorKey(), null);
+              }
+            },
           ),
         ),
       );
+
+  String _getIndicatorKey() => runtimeType.toString();
+
+  IndicatorSeriesBuilder createIndicatorSeries();
+}
+
+class MAIndicatorItem extends IndicatorItem<MASeries> {
+  MAIndicatorItem({
+    Key key,
+    List<Tick> ticks,
+    OnAddIndicator onAddIndicator,
+  }) : super(
+          key: key,
+          title: 'MAIndicator',
+          ticks: ticks,
+          onAddIndicator: onAddIndicator,
+        );
+
+  @override
+  _IndicatorItemState createIndicatorItemState() => MAIndicatorSeriesState();
+}
+
+class MAIndicatorSeriesState extends _IndicatorItemState {
+  @override
+  IndicatorSeriesBuilder createIndicatorSeries() => (List<Tick> ticks) {
+        return MASeries(ticks);
+      };
 }
