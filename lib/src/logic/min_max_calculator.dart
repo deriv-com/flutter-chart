08e0e1db799d2bbc854be80559e16aa36223b82c
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-07 15:31:41 +0800

    Add entry interface

--- lib/src/logic/min_max_calculator.dart
+++ lib/src/logic/min_max_calculator.dart
@@ -1,18 +1,18 @@
-/// Accepts a list of entries and calculates min/max values for that list.
+/// Accepts a list of entries sorted by time and calculates min/max values for that list.
 /// Reuses work done when a new list is supplied.
 /// TODO: Use OHLC interface?
 ///
 /// Keep one instance for each unique `Series` or list of entries where visible entries change over time.
-class MinMaxCalculator<T extends Tick> {
+class MinMaxCalculator<T implements MinMaxCalculatorEntry> {
   /// Creates and calculates min/max immediately.
   MinMaxCalculator(List<T> visibleEntries) : _visibleEntries = visibleEntries;
 
   List<T> _visibleEntries;
 
-  /// Min value of current visible entries.
+  /// Minimum value of current visible entries.
   double get min;
 
-  /// Max value of current visible entries.
+  /// Maximum value of current visible entries.
   double get max;
 
   /// Updates a list of entries and efficiently recalculates min/max.
@@ -20,4 +20,13 @@ class MinMaxCalculator<T extends Tick> {
 }
 
 /// Interface that should be implemented by all entries to `MinMaxCalculator`.
-abstract class MinMaxCalculatorEntry {}
+abstract class MinMaxCalculatorEntry {
+  /// Epoch time of this entry.
+  int epoch;
+
+  /// Minumum value of this entry.
+  double min;
+
+  /// Maximum value of this entry.
+  double max;
+}
