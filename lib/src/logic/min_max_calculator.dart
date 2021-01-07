06fb0653113d83f8d863be4f893d0cc86473a7c7
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-07 12:40:53 +0800

    Pass type

--- lib/src/logic/min_max_calculator.dart
+++ lib/src/logic/min_max_calculator.dart
@@ -1,3 +1,4 @@
 /// Accepts a list of entries and calculates min/max values for that list.
 /// Reuses work done when new list is supplied.
-class MinMaxCalculator {}
+/// TODO: Use OHLC interface?
+class MinMaxCalculator<T extends Tick> {}
