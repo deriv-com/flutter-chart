4099569e632080bc661eaa886af5f1210fd5ca41
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-07 12:43:20 +0800

    Grammar 2

--- lib/src/logic/min_max_calculator.dart
+++ lib/src/logic/min_max_calculator.dart
@@ -1,5 +1,5 @@
 /// Accepts a list of entries and calculates min/max values for that list.
-/// Reuses work done when new list is supplied.
+/// Reuses work done when a new list is supplied.
 /// TODO: Use OHLC interface?
 ///
 /// Keep one instance for each unique `Series` or list of data with a sliding window.
