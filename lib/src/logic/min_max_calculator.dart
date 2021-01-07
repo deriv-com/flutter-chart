33b41e346fe8e3b8dfab12b6b7df609a6b2cd362
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-07 12:42:52 +0800

    Improve docs

--- lib/src/logic/min_max_calculator.dart
+++ lib/src/logic/min_max_calculator.dart
@@ -1,4 +1,6 @@
 /// Accepts a list of entries and calculates min/max values for that list.
 /// Reuses work done when new list is supplied.
 /// TODO: Use OHLC interface?
+///
+/// Keep one instance for each unique `Series` or list of data with sliding window.
 class MinMaxCalculator<T extends Tick> {}
