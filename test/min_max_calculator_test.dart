d66c7c86439a3f3573543030b70f6bed28467c1c
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-12 12:23:19 +0800

    Fix lints

--- test/min_max_calculator_test.dart
+++ test/min_max_calculator_test.dart
@@ -5,15 +5,14 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('MinMaxCalculator', () {
     test('calculates min/max on initial entries', () {
-      final calculator = MinMaxCalculator<Tick>();
-
-      calculator.updateVisibleEntries(<Tick>[
-        const Tick(epoch: 123, quote: 10),
-        const Tick(epoch: 133, quote: 8),
-        const Tick(epoch: 143, quote: 13),
-        const Tick(epoch: 153, quote: 192),
-        const Tick(epoch: 163, quote: 9),
-      ]);
+      final MinMaxCalculator calculator = MinMaxCalculator<Tick>()
+        ..updateVisibleEntries(<Tick>[
+          const Tick(epoch: 123, quote: 10),
+          const Tick(epoch: 133, quote: 8),
+          const Tick(epoch: 143, quote: 13),
+          const Tick(epoch: 153, quote: 192),
+          const Tick(epoch: 163, quote: 9),
+        ]);
 
       expect(calculator.min, 8);
       expect(calculator.max, 192);
