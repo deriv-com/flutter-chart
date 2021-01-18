cbcf9b719695639173d6ffcf1b1b5e2ac49653b9
Samin-FS <samin@firstsource.tech>
2021-01-18 11:23:25 +0330

    fix review comments

--- lib/src/logic/indicators/calculations/helper_indicators/ohlc4_indicator.dart
+++ lib/src/logic/indicators/calculations/helper_indicators/ohlc4_indicator.dart
@@ -2,7 +2,7 @@ import 'package:deriv_chart/src/models/tick.dart';
 
 import '../../indicator.dart';
 
-/// A helper indicator to get the [(H + L+ 2* C) / 4] value of a list of [Tick]
+/// A helper indicator to get the [(O+ H + L+ C) / 4] value of a list of [Tick]
 class OHLC4Indicator extends Indicator {
   /// Initializes
   OHLC4Indicator(List<Tick> entries) : super(entries);
