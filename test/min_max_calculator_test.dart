6207f8cb461ea09cbe87887a52b62c355f4efbcd
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-18 16:36:07 +0800

    Fix tests

--- test/min_max_calculator_test.dart
+++ test/min_max_calculator_test.dart
@@ -2,17 +2,21 @@ import 'package:deriv_chart/deriv_chart.dart';
 import 'package:deriv_chart/src/logic/min_max_calculator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+double minValueOf(Tick t) => t.quote;
+double maxValueOf(Tick t) => t.quote;
+
 void main() {
   group('MinMaxCalculator', () {
     test('calculates min/max on initial entries', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 10),
-          const Tick(epoch: 133, quote: 8),
-          const Tick(epoch: 143, quote: 13),
-          const Tick(epoch: 153, quote: 192),
-          const Tick(epoch: 163, quote: 9),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 10),
+              const Tick(epoch: 133, quote: 8),
+              const Tick(epoch: 143, quote: 13),
+              const Tick(epoch: 153, quote: 192),
+              const Tick(epoch: 163, quote: 9),
+            ]);
 
       expect(calculator.min, 8);
       expect(calculator.max, 192);
@@ -27,32 +31,33 @@ void main() {
         const Tick(epoch: 163, quote: 9),
       ];
 
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(testEntries);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)..calculate(testEntries);
 
       expect(calculator.min, 8);
       expect(calculator.max, 192);
 
-      calculator.updateVisibleEntries(testEntries);
+      calculator.calculate(testEntries);
 
       expect(calculator.min, 8);
       expect(calculator.max, 192);
     });
 
     test('updates min if new entries are scrolled forward by 1', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 11),
-          const Tick(epoch: 124, quote: 39),
-          const Tick(epoch: 125, quote: 45),
-          const Tick(epoch: 126, quote: 5),
-          const Tick(epoch: 127, quote: 23),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 11),
+              const Tick(epoch: 124, quote: 39),
+              const Tick(epoch: 125, quote: 45),
+              const Tick(epoch: 126, quote: 5),
+              const Tick(epoch: 127, quote: 23),
+            ]);
 
       expect(calculator.min, 5);
       expect(calculator.max, 45);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 124, quote: 39),
         const Tick(epoch: 125, quote: 45),
         const Tick(epoch: 126, quote: 5),
@@ -65,19 +70,20 @@ void main() {
     });
 
     test('updates max if new entries are scrolled forward by 1', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 11),
-          const Tick(epoch: 124, quote: 39),
-          const Tick(epoch: 125, quote: 45),
-          const Tick(epoch: 126, quote: 5),
-          const Tick(epoch: 127, quote: 23),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 11),
+              const Tick(epoch: 124, quote: 39),
+              const Tick(epoch: 125, quote: 45),
+              const Tick(epoch: 126, quote: 5),
+              const Tick(epoch: 127, quote: 23),
+            ]);
 
       expect(calculator.min, 5);
       expect(calculator.max, 45);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 124, quote: 39),
         const Tick(epoch: 125, quote: 45),
         const Tick(epoch: 126, quote: 5),
@@ -90,19 +96,20 @@ void main() {
     });
 
     test('updates min/max if new entries are scrolled forward by 2', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 1, quote: 4),
-          const Tick(epoch: 2, quote: 290),
-          const Tick(epoch: 3, quote: 87),
-          const Tick(epoch: 4, quote: 7),
-          const Tick(epoch: 5, quote: 6),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 1, quote: 4),
+              const Tick(epoch: 2, quote: 290),
+              const Tick(epoch: 3, quote: 87),
+              const Tick(epoch: 4, quote: 7),
+              const Tick(epoch: 5, quote: 6),
+            ]);
 
       expect(calculator.min, 4);
       expect(calculator.max, 290);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 3, quote: 87),
         const Tick(epoch: 4, quote: 7),
         const Tick(epoch: 5, quote: 6),
@@ -115,19 +122,20 @@ void main() {
     });
 
     test('updates min if new entries are scrolled backward by 1', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 11),
-          const Tick(epoch: 124, quote: 39),
-          const Tick(epoch: 125, quote: 45),
-          const Tick(epoch: 126, quote: 5),
-          const Tick(epoch: 127, quote: 23),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 11),
+              const Tick(epoch: 124, quote: 39),
+              const Tick(epoch: 125, quote: 45),
+              const Tick(epoch: 126, quote: 5),
+              const Tick(epoch: 127, quote: 23),
+            ]);
 
       expect(calculator.min, 5);
       expect(calculator.max, 45);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 122, quote: 2),
         const Tick(epoch: 123, quote: 11),
         const Tick(epoch: 124, quote: 39),
@@ -140,19 +148,20 @@ void main() {
     });
 
     test('updates max if new entries are scrolled backward by 1', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 11),
-          const Tick(epoch: 124, quote: 39),
-          const Tick(epoch: 125, quote: 45),
-          const Tick(epoch: 126, quote: 5),
-          const Tick(epoch: 127, quote: 23),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 11),
+              const Tick(epoch: 124, quote: 39),
+              const Tick(epoch: 125, quote: 45),
+              const Tick(epoch: 126, quote: 5),
+              const Tick(epoch: 127, quote: 23),
+            ]);
 
       expect(calculator.min, 5);
       expect(calculator.max, 45);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 122, quote: 81),
         const Tick(epoch: 123, quote: 11),
         const Tick(epoch: 124, quote: 39),
@@ -165,19 +174,20 @@ void main() {
     });
 
     test('updates min/max if new entries are scrolled backward by 2', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 123, quote: 11),
-          const Tick(epoch: 124, quote: 39),
-          const Tick(epoch: 125, quote: 45),
-          const Tick(epoch: 126, quote: 5),
-          const Tick(epoch: 127, quote: 23),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 123, quote: 11),
+              const Tick(epoch: 124, quote: 39),
+              const Tick(epoch: 125, quote: 45),
+              const Tick(epoch: 126, quote: 5),
+              const Tick(epoch: 127, quote: 23),
+            ]);
 
       expect(calculator.min, 5);
       expect(calculator.max, 45);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 121, quote: 120),
         const Tick(epoch: 122, quote: 2),
         const Tick(epoch: 123, quote: 11),
@@ -190,20 +200,21 @@ void main() {
     });
 
     test('updates min/max if new entries are zoomed in', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 1, quote: 10),
-          const Tick(epoch: 2, quote: 20),
-          const Tick(epoch: 3, quote: 30),
-          const Tick(epoch: 4, quote: 40),
-          const Tick(epoch: 5, quote: 50),
-          const Tick(epoch: 6, quote: 60),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 1, quote: 10),
+              const Tick(epoch: 2, quote: 20),
+              const Tick(epoch: 3, quote: 30),
+              const Tick(epoch: 4, quote: 40),
+              const Tick(epoch: 5, quote: 50),
+              const Tick(epoch: 6, quote: 60),
+            ]);
 
       expect(calculator.min, 10);
       expect(calculator.max, 60);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 2, quote: 20),
         const Tick(epoch: 3, quote: 30),
         const Tick(epoch: 4, quote: 40),
@@ -215,18 +226,19 @@ void main() {
     });
 
     test('updates min/max if new entries are zoomed out', () {
-      final MinMaxCalculator<Tick> calculator = MinMaxCalculator<Tick>()
-        ..updateVisibleEntries(<Tick>[
-          const Tick(epoch: 2, quote: 20),
-          const Tick(epoch: 3, quote: 30),
-          const Tick(epoch: 4, quote: 40),
-          const Tick(epoch: 5, quote: 50),
-        ]);
+      final MinMaxCalculator calculator =
+          MinMaxCalculator(minValueOf, maxValueOf)
+            ..calculate(<Tick>[
+              const Tick(epoch: 2, quote: 20),
+              const Tick(epoch: 3, quote: 30),
+              const Tick(epoch: 4, quote: 40),
+              const Tick(epoch: 5, quote: 50),
+            ]);
 
       expect(calculator.min, 20);
       expect(calculator.max, 50);
 
-      calculator.updateVisibleEntries(<Tick>[
+      calculator.calculate(<Tick>[
         const Tick(epoch: 1, quote: 10),
         const Tick(epoch: 2, quote: 20),
         const Tick(epoch: 3, quote: 30),
