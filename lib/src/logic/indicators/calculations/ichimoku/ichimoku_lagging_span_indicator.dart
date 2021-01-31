c63cf0e77513f0f7fc51dff5abe0174975f73b53
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-01-31 12:25:02 +0330

    fix: change ichimoku lagging span indicator ancestor to cached indicator

--- lib/src/logic/indicators/calculations/ichimoku/ichimoku_lagging_span_indicator.dart
+++ lib/src/logic/indicators/calculations/ichimoku/ichimoku_lagging_span_indicator.dart
@@ -1,10 +1,18 @@
+import 'package:deriv_chart/src/logic/indicators/cached_indicator.dart';
 import 'package:deriv_chart/src/logic/indicators/calculations/helper_indicators/close_value_inidicator.dart';
 import 'package:deriv_chart/src/models/ohlc.dart';
+import 'package:deriv_chart/src/models/tick.dart';
 
 /// An `indicator` to calculate the `close` value the given number of offsets ago.
-class IchimokuLaggingSpanIndicator extends CloseValueIndicator {
+class IchimokuLaggingSpanIndicator extends CachedIndicator {
   /// Initializes an [IchimokuLaggingSpanIndicator].
   IchimokuLaggingSpanIndicator(
     List<OHLC> entries,
-  ) : super(entries);
+  )   : _closeValueIndicator = CloseValueIndicator(entries),
+        super(entries);
+
+  @override
+  Tick calculate(int index) => _closeValueIndicator.getValue(index);
+
+  final CloseValueIndicator _closeValueIndicator;
 }
