05e441909575c761facdf0a9e1adcfaf4f80724a
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-02-01 11:12:03 +0330

    Removed offset propert from Indicator class

--- lib/src/logic/indicators/indicator.dart
+++ lib/src/logic/indicators/indicator.dart
@@ -6,24 +6,13 @@ import 'package:deriv_chart/src/models/tick.dart';
 /// Holds common functionalities of indicators like getting epoch for an index or handling indicator's offset.
 abstract class Indicator {
   /// Initializes
-  Indicator(this.entries, {this.offset = 10})
-      : _entriesInterval = entries.length > 2
-            ? entries.last.epoch - entries[entries.length - 2].epoch
-            : 1000;
+  Indicator(this.entries);
 
   /// List of data to calculate indicator values on.
   final List<OHLC> entries;
 
-  /// The offset of the indicator.
-  final int offset;
-
-  /// Offset steps.
-  final int _entriesInterval;
-
   /// Gets the epoch of the given [index]
-  // TODO(Ramin): Handle indicator offset here.
-  int getEpochOfIndex(int index) =>
-      entries[index].epoch + (offset * _entriesInterval);
+  int getEpochOfIndex(int index) => entries[index].epoch;
 
   /// Value of the indicator for the given [index].
   Tick getValue(int index);
