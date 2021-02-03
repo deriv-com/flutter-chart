557898b166db7722994bb1a88c33c188bfa5e95d
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-03 12:01:19 +0800

    Outline params for color picker

--- lib/src/widgets/color_picker.dart
+++ lib/src/widgets/color_picker.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ColorPicker extends StatelessWidget {
   const ColorPicker({Key key}) : super(key: key);
 
+  // final ValueChanged<Color> onChanged;
+
   @override
   Widget build(BuildContext context) {
     // LayoutBuilder(builder: ,);
