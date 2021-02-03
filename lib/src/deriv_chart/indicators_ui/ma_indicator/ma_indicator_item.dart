241326490df838839065b0e9caa938afb38c6a5d
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-03 16:01:32 +0800

    Move to folder

--- lib/src/deriv_chart/indicators_ui/ma_indicator/ma_indicator_item.dart
+++ lib/src/deriv_chart/indicators_ui/ma_indicator/ma_indicator_item.dart
@@ -3,7 +3,7 @@ import 'package:deriv_chart/src/helpers/helper_functions.dart';
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/ma_series.dart';
 import 'package:deriv_chart/src/models/tick.dart';
 import 'package:deriv_chart/src/theme/painting_styles/line_style.dart';
-import 'package:deriv_chart/src/widgets/color_button.dart';
+import 'package:deriv_chart/src/widgets/color_picker/color_button.dart';
 import 'package:flutter/material.dart';
 
 import '../callbacks.dart';
