0d26d273e0457c93330d8abdea40ff301faf74b1
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-03 15:44:44 +0800

    Add radius to splash

--- lib/src/widgets/color_button.dart
+++ lib/src/widgets/color_button.dart
@@ -18,6 +18,7 @@ class ColorButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      borderRadius: BorderRadius.circular(4),
       onTap: () {},
       child: SizedBox(
         width: 44,
