c256995b6399ad72085a99cf2b36fd61e6f16bbe
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-03 10:12:09 +0800

    Setup color picker

---
+++ lib/src/widgets/color_picker.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker extends StatelessWidget {
+  const ColorPicker({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // LayoutBuilder(builder: ,);
+    return GridView.count(crossAxisCount: null);
+  }
+}
