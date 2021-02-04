f80d2f2fb2e37c822ee8c61facd1186a6b7549be
Samin-FS <samin@firstsource.tech>
2021-02-04 13:27:05 +0330

    fix tests

--- test/logic/indicators/calculations/zigzag_indicator_test.dart
+++ test/logic/indicators/calculations/zigzag_indicator_test.dart
@@ -12,17 +12,17 @@ void main() {
     setUpAll(() {
       ticks = const <Tick>[
         Tick(epoch: 1, quote: 64.75),
-        Tick(epoch: 2, quote: 64.79),
+        Tick(epoch: 2, quote: 65.79),
         Tick(epoch: 3, quote: 64.73),
         Tick(epoch: 4, quote: 64.73),
         Tick(epoch: 5, quote: 65.55),
-        Tick(epoch: 6, quote: 63.19),
+        Tick(epoch: 6, quote: 66.59),
         Tick(epoch: 7, quote: 63.61),
         Tick(epoch: 8, quote: 63.65),
         Tick(epoch: 9, quote: 62.15),
         Tick(epoch: 10, quote: 63.37),
         Tick(epoch: 11, quote: 61.33),
-        Tick(epoch: 12, quote: 61.51),
+        Tick(epoch: 12, quote: 62.51),
       ];
     });
 
@@ -30,14 +30,14 @@ void main() {
       final ZigZagIndicator indicator =
           ZigZagIndicator(CloseValueIndicator(ticks), 1);
 
-      expect(indicator.getValue(0).quote, 64.75);
-      expect(indicator.getValue(1).quote.isNaN, true);
+      expect(indicator.getValue(0).quote.isNaN, true);
+      expect(indicator.getValue(1).quote, 65.79);
       expect(indicator.getValue(2).quote.isNaN, true);
       expect(indicator.getValue(3).quote.isNaN, true);
-      expect(indicator.getValue(4).quote, 65.55);
-      expect(indicator.getValue(5).quote, 63.19);
-      expect(indicator.getValue(6).quote.isNaN, true);
-      expect(indicator.getValue(8).quote, 62.15);
+      expect(indicator.getValue(5).quote, 66.59);
+      expect(indicator.getValue(7).quote.isNaN, true);
+      expect(indicator.getValue(10).quote, 61.33);
+      expect(indicator.getValue(11).quote, 62.51);
     });
   });
 }
