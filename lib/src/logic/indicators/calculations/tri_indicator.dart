e7fe833010e201f226e6e432252695e4cb188d3e
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-02-08 14:10:59 +0330

    feat: add TRIndicator

---
+++ lib/src/logic/indicators/calculations/tri_indicator.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:deriv_chart/src/logic/indicators/cached_indicator.dart';
+import 'package:deriv_chart/src/models/ohlc.dart';
+import 'package:deriv_chart/src/models/tick.dart';
+
+/// True range indicator.
+class TRIndicator extends CachedIndicator {
+  /// Initializes a true range indicator.
+  TRIndicator(List<OHLC> entries) : super(entries);
+
+  @override
+  Tick calculate(int index) {
+    final double ts = entries[index].high - entries[index].low;
+
+    final double ys =
+        index == 0 ? 0 : entries[index].high - entries[index].close;
+    final double yst =
+        index == 0 ? 0 : entries[index].close - entries[index].low;
+
+    return max(
+      ts.abs(),
+      max(
+        ys.abs(),
+        yst.abs(),
+      ),
+    );
+  }
+}
