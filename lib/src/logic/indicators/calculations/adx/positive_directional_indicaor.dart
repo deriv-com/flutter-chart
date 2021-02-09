a399e831a926a3905ed910dce1ba93b4737fccb9
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-02-09 12:39:48 +0330

    feat: add positive directional indicator

---
+++ lib/src/logic/indicators/calculations/adx/positive_directional_indicaor.dart
@@ -0,0 +1,29 @@
+import 'package:deriv_chart/src/logic/indicators/cached_indicator.dart';
+import 'package:deriv_chart/src/logic/indicators/calculations/atr_indicator.dart';
+import 'package:deriv_chart/src/logic/indicators/calculations/helper_indicators/positive_dm_indicator.dart';
+import 'package:deriv_chart/src/logic/indicators/calculations/mma_indicator.dart';
+import 'package:deriv_chart/src/models/ohlc.dart';
+import 'package:deriv_chart/src/models/tick.dart';
+import 'package:flutter/foundation.dart';
+
+/// Negative Directional indicator. Part of the Directional Movement System.
+class PositiveDirectionalIndicator extends CachedIndicator {
+  /// Initializes Negative Directional indicator.
+  PositiveDirectionalIndicator(
+    List<OHLC> entries, {
+    @required int period,
+  })  : _avgPlusDMIndicator =
+            MMAIndicator(PositiveDMIndicator(entries), period),
+        _atrIndicator = ATRIndicator(entries, period: period),
+        super(entries);
+
+  final MMAIndicator _avgPlusDMIndicator;
+  final ATRIndicator _atrIndicator;
+
+  @override
+  Tick calculate(int index) => Tick(
+      epoch: getEpochOfIndex(index),
+      quote: (_avgPlusDMIndicator.getValue(index).quote /
+              _atrIndicator.getValue(index).quote) *
+          100);
+}
