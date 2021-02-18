7e1625aac1df31b427ad71dbd4ee73f0fc98a4d6
Samin-FS <samin@firstsource.tech>
2021-02-18 14:38:07 +0330

    removed unsed import

--- deriv_technical_analysis/test/indicators/calculations/smma_indicator_test.dart
+++ deriv_technical_analysis/test/indicators/calculations/smma_indicator_test.dart
@@ -1,5 +1,4 @@
 import 'package:deriv_technical_analysis/src/indicators/calculations/helper_indicators/close_value_inidicator.dart';
-import 'package:deriv_technical_analysis/src/indicators/calculations/mma_indicator.dart';
 import 'package:deriv_technical_analysis/src/indicators/calculations/smma_indicator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
