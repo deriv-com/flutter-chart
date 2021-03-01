7cc722e87c94672db4c08818883ffed9de29940a
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-01 11:51:25 +0800

    Indicator serialization interface

--- lib/src/deriv_chart/indicators_ui/indicator_config.dart
+++ lib/src/deriv_chart/indicators_ui/indicator_config.dart
@@ -10,6 +10,24 @@ abstract class IndicatorConfig {
   /// Initializes
   const IndicatorConfig();
 
+  /// Create a concrete indicator config implementation based on JSON.
+  factory IndicatorConfig.fromJson(Map<String, dynamic> json) {
+    if (!json.containsKey('name')) {
+      throw ArgumentError.value(json, 'json', 'Missing indicator name.');
+    }
+
+    switch (json['name']) {
+      default:
+        throw ArgumentError.value(json, 'json', 'Unidentified indicator name.');
+    }
+  }
+
+  /// Unique name for this indicator. Used as key in key-value storage.
+  String get name;
+
+  /// Serialization to JSON. Used as value in key-value storage.
+  Map<String, dynamic> toJson();
+
   /// Indicators supported field types
   static final Map<String, FieldIndicatorBuilder> supportedFieldTypes =
       <String, FieldIndicatorBuilder>{
