b6f9e38e019ed46c76ade4c9a2041dcea280963f
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-11 12:28:48 +0800

    Serialize DonchianChannelIndicatorConfig

--- lib/src/deriv_chart/indicators_ui/donchian_channel/donchian_channel_indicator_config.dart
+++ lib/src/deriv_chart/indicators_ui/donchian_channel/donchian_channel_indicator_config.dart
@@ -1,11 +1,17 @@
 import 'package:deriv_chart/deriv_chart.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/indicator_config.dart';
+import 'package:deriv_chart/src/helpers/color_converter.dart';
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/donchian_channels_series.dart';
 import 'package:deriv_chart/src/logic/chart_series/series.dart';
 import 'package:deriv_chart/src/models/indicator_input.dart';
 import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'donchian_channel_indicator_config.g.dart';
 
 /// Donchian Channel Indicator Config
+@JsonSerializable()
+@ColorConverter()
 class DonchianChannelIndicatorConfig extends IndicatorConfig {
   /// Initializes
   const DonchianChannelIndicatorConfig({
@@ -17,6 +23,17 @@ class DonchianChannelIndicatorConfig extends IndicatorConfig {
     this.lowerLineStyle = const LineStyle(color: Colors.green),
     this.fillColor = Colors.white12,
   }) : super();
+
+  /// Initializes from JSON.
+  factory DonchianChannelIndicatorConfig.fromJson(Map<String, dynamic> json) =>
+      _$DonchianChannelIndicatorConfigFromJson(json);
+
+  /// Unique name for this indicator.
+  static const String name = 'donchian_channel';
+
+  @override
+  Map<String, dynamic> toJson() => _$DonchianChannelIndicatorConfigToJson(this)
+    ..putIfAbsent(IndicatorConfig.nameKey, () => name);
 
   /// Number of last candles used to calculate the highest value.
   final int highPeriod;
