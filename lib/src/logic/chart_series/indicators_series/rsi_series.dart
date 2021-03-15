439c66d10ca975fe62fb4b3e83536a59e4bd04e0
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-03-15 15:16:21 +0330

    fix: remove redundant code

--- lib/src/logic/chart_series/indicators_series/rsi_series.dart
+++ lib/src/logic/chart_series/indicators_series/rsi_series.dart
@@ -1,17 +1,12 @@
 import 'package:deriv_chart/deriv_chart.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/rsi/rsi_indicator_config.dart';
-import 'package:deriv_chart/src/logic/chart_data.dart';
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/abstract_single_indicator_series.dart';
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/models/rsi_options.dart';
-import 'package:deriv_chart/src/logic/chart_series/line_series/line_series.dart';
 import 'package:deriv_chart/src/logic/chart_series/line_series/oscillator_line_painter.dart';
 import 'package:deriv_chart/src/logic/chart_series/series.dart';
 import 'package:deriv_chart/src/logic/chart_series/series_painter.dart';
-import 'package:deriv_chart/src/models/animation_info.dart';
-import 'package:deriv_chart/src/models/chart_config.dart';
 import 'package:deriv_chart/src/models/indicator_input.dart';
 import 'package:deriv_chart/src/models/tick.dart';
-import 'package:deriv_chart/src/theme/chart_theme.dart';
 import 'package:deriv_technical_analysis/deriv_technical_analysis.dart';
 import 'package:flutter/material.dart';
 
@@ -38,8 +33,6 @@ class RSISeries extends AbstractSingleIndicatorSeries {
   })  : _inputIndicator = inputIndicator,
         super(inputIndicator, id, rsiOptions);
 
-  LineSeries _rsiSeries;
-
   final Indicator<Tick> _inputIndicator;
 
   /// Configuration of RSI.
@@ -56,46 +49,6 @@ class RSISeries extends AbstractSingleIndicatorSeries {
         secondaryHorizontalLinesStyle: config.zeroHorizontalLinesStyle,
         topHorizontalLine: config.overBoughtPrice,
       );
-
-  @override
-  bool didUpdate(ChartData oldData) {
-    final RSISeries oldSeries = oldData;
-
-    final bool rsiUpdated = _rsiSeries.didUpdate(oldSeries?._rsiSeries);
-
-    return rsiUpdated;
-  }
-
-  @override
-  void onUpdate(int leftEpoch, int rightEpoch) {
-    _rsiSeries.update(leftEpoch, rightEpoch);
-  }
-
-  @override
-  List<double> recalculateMinMax() => <double>[
-        _rsiSeries.minValue,
-        _rsiSeries.maxValue,
-      ];
-
-  @override
-  void paint(
-    Canvas canvas,
-    Size size,
-    double Function(int) epochToX,
-    double Function(double) quoteToY,
-    AnimationInfo animationInfo,
-    ChartConfig chartConfig,
-    ChartTheme theme,
-  ) {
-    _rsiSeries.paint(
-        canvas, size, epochToX, quoteToY, animationInfo, chartConfig, theme);
-  }
-
-  @override
-  int getMaxEpoch() => _rsiSeries.getMaxEpoch();
-
-  @override
-  int getMinEpoch() => _rsiSeries.getMinEpoch();
 
   @override
   CachedIndicator<Tick> initializeIndicator() =>
