1eb7b35035f94d71bbe25dce4c0548fd01b41a2e
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-17 16:29:15 +0800

    Serialize alligator

--- lib/src/deriv_chart/indicators_ui/indicator_config.dart
+++ lib/src/deriv_chart/indicators_ui/indicator_config.dart
@@ -1,3 +1,4 @@
+import 'package:deriv_chart/src/deriv_chart/indicators_ui/alligator/alligator_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/bollinger_bands/bollinger_bands_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/donchian_channel/donchian_channel_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/indicator_item.dart';
@@ -32,6 +33,8 @@ abstract class IndicatorConfig {
         return BollingerBandsIndicatorConfig.fromJson(json);
       case DonchianChannelIndicatorConfig.name:
         return DonchianChannelIndicatorConfig.fromJson(json);
+      case AlligatorIndicatorConfig.name:
+        return AlligatorIndicatorConfig.fromJson(json);
       // Add new indicators here.
       default:
         throw ArgumentError.value(json, 'json', 'Unidentified indicator name.');
--- lib/src/deriv_chart/indicators_ui/indicators_dialog.dart
+++ lib/src/deriv_chart/indicators_ui/indicators_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:deriv_chart/src/deriv_chart/indicators_ui/alligator/alligator_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/bollinger_bands/bollinger_bands_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/donchian_channel/donchian_channel_indicator_config.dart';
 import 'package:deriv_chart/src/deriv_chart/indicators_ui/indicator_config.dart';
@@ -47,6 +48,10 @@ class _IndicatorsDialogState extends State<IndicatorsDialog> {
                   const DropdownMenuItem<IndicatorConfig>(
                     child: Text('Donchian channel'),
                     value: DonchianChannelIndicatorConfig(),
+                  ),
+                  const DropdownMenuItem<IndicatorConfig>(
+                    child: Text('Alligator'),
+                    value: AlligatorIndicatorConfig(),
                   ),
                 ],
                 onChanged: (IndicatorConfig config) {
