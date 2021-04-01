69683f4f280f5f84dceea68fe55c8e8b91b2230d
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-04-01 18:55:29 +0430

    Update ScatterPainter to follow DataSeries.visibleEntries change

--- lib/src/logic/chart_series/scatter/scatter_painter.dart
+++ lib/src/logic/chart_series/scatter/scatter_painter.dart
@@ -26,11 +26,13 @@ class ScatterPainter extends DataPainter<DataSeries<Tick>> {
       ..color = style.color
       ..style = PaintingStyle.fill;
 
-    for (int i = 0; i < series.visibleEntries.length; i++) {
-      final Tick tick = series.visibleEntries[i];
+    for (int i = series.visibleEntries.startIndex;
+        i < series.visibleEntries.endIndex;
+        i++) {
+      final Tick tick = series.entries[i];
 
       if (!tick.quote.isNaN) {
-        final double x = epochToX(tick.epoch);
+        final double x = epochToX(getEpochOf(tick, i));
         final double y = quoteToY(tick.quote);
         canvas.drawCircle(Offset(x, y), style.radius, dotPaint);
       }
