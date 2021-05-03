d454e564c4bc1f7997c2c52b8eb527e47dc46b8f
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-05-03 12:49:31 +0430

    Remove redundant async in indicator_dialog

--- lib/src/deriv_chart/indicators_ui/indicators_dialog.dart
+++ lib/src/deriv_chart/indicators_ui/indicators_dialog.dart
@@ -112,7 +112,7 @@ class _IndicatorsDialogState extends State<IndicatorsDialog> {
                   repo.indicators[index].getItem(
                 (IndicatorConfig updatedConfig) =>
                     repo.updateAt(index, updatedConfig),
-                () async {
+                () {
                   repo.removeAt(index);
                   setState(() {});
                 },
