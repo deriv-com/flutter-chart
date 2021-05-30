def18814f3d4ed54de4d198b521d6d50e9c0ed51
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-05-30 18:01:52 +0430

    Add overbought/sold lines

--- lib/src/logic/chart_series/indicators_series/williams_r_series.dart
+++ lib/src/logic/chart_series/indicators_series/williams_r_series.dart
@@ -1,10 +1,12 @@
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/abstract_single_indicator_series.dart';
 import 'package:deriv_chart/src/logic/chart_series/indicators_series/models/williams_r_options.dart';
-import 'package:deriv_chart/src/logic/chart_series/line_series/line_painter.dart';
+import 'package:deriv_chart/src/logic/chart_series/line_series/oscillator_line_painter.dart';
 import 'package:deriv_chart/src/logic/chart_series/series.dart';
 import 'package:deriv_chart/src/logic/chart_series/series_painter.dart';
 import 'package:deriv_chart/src/models/tick.dart';
+import 'package:deriv_chart/src/theme/painting_styles/line_style.dart';
 import 'package:deriv_technical_analysis/deriv_technical_analysis.dart';
+import 'package:flutter/material.dart';
 
 /// WilliamsRSeries
 class WilliamsRSeries extends AbstractSingleIndicatorSeries {
@@ -12,6 +14,10 @@ class WilliamsRSeries extends AbstractSingleIndicatorSeries {
   WilliamsRSeries(
     this._indicatorDataInput,
     this._options, {
+    this.overboughtValue = -20,
+    this.oversoldValue = -80,
+    this.overboughtSoldLineStyles =
+        const LineStyle(color: Colors.white, thickness: 0.5),
     String id,
   }) : super(
           CloseValueIndicator<Tick>(_indicatorDataInput),
@@ -23,8 +29,24 @@ class WilliamsRSeries extends AbstractSingleIndicatorSeries {
 
   final WilliamsROptions _options;
 
+  /// Overbought value
+  final double overboughtValue;
+
+  /// Oversold value
+  final double oversoldValue;
+
+  /// The line style for overbought/sold horizontal lines.
+  final LineStyle overboughtSoldLineStyles;
+
   @override
-  SeriesPainter<Series> createPainter() => LinePainter(this);
+  SeriesPainter<Series> createPainter() => OscillatorLinePainter(
+        this,
+        topHorizontalLine: overboughtValue,
+        bottomHorizontalLine: oversoldValue,
+        secondaryHorizontalLinesStyle: overboughtSoldLineStyles,
+        // TODO(NA): Zero line style will be removed from OscillatorLinePainter.
+        mainHorizontalLinesStyle: overboughtSoldLineStyles,
+      );
 
   @override
   CachedIndicator<Tick> initializeIndicator() => WilliamsRIndicator<Tick>(
