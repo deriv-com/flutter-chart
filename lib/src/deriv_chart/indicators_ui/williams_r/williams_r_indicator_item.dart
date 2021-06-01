000b4589aec536d4615f18a942fc475f93747951
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-06-01 14:37:44 +0430

    Move fieldWidget to a separate file

--- lib/src/deriv_chart/indicators_ui/williams_r/williams_r_indicator_item.dart
+++ lib/src/deriv_chart/indicators_ui/williams_r/williams_r_indicator_item.dart
@@ -3,6 +3,7 @@ import 'package:deriv_chart/deriv_chart.dart';
 
 import 'package:flutter/material.dart';
 
+import '../../widgets/field_widget.dart';
 import '../callbacks.dart';
 import '../indicator_config.dart';
 import '../indicator_item.dart';
@@ -104,41 +105,4 @@ class WilliamsRIndicatorItemState
       _overSoldPrice ??
       (widget.config as WilliamsRIndicatorConfig)?.overSoldPrice ??
       -80;
-}
-
-/// Field widget
-class FieldWidget extends StatelessWidget {
-  /// Initializes
-  const FieldWidget({
-    @required this.initialValue,
-    this.onValueChanged,
-    this.label = '',
-    Key key,
-  }) : super(key: key);
-
-  /// Period's initial value
-  final String initialValue;
-
-  /// Will be called whenever the field's value has changed.
-  final Function(String) onValueChanged;
-
-  /// The label of the field.
-  final String label;
-
-  @override
-  Widget build(BuildContext context) => Row(
-        children: <Widget>[
-          Text(label, style: const TextStyle(fontSize: 10)),
-          const SizedBox(width: 4),
-          SizedBox(
-            width: 20,
-            child: TextFormField(
-              style: const TextStyle(fontSize: 10),
-              initialValue: initialValue,
-              keyboardType: TextInputType.number,
-              onChanged: onValueChanged,
-            ),
-          ),
-        ],
-      );
 }
