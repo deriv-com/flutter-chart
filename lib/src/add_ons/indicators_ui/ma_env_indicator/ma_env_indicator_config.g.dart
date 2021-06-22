fd23925272ccdefb40d2922f703c4ac1adcca5a2
Samin-FS <samin@firstsource.tech>
2021-06-22 10:10:34 +0430

    run flutter pub run build_runner

--- lib/src/add_ons/indicators_ui/ma_env_indicator/ma_env_indicator_config.g.dart
+++ lib/src/add_ons/indicators_ui/ma_env_indicator/ma_env_indicator_config.g.dart
@@ -66,6 +66,7 @@ const _$MovingAverageTypeEnumMap = {
   MovingAverageType.weighted: 'weighted',
   MovingAverageType.hull: 'hull',
   MovingAverageType.zeroLag: 'zeroLag',
+  MovingAverageType.triangular: 'triangular',
 };
 
 const _$ShiftTypeEnumMap = {
