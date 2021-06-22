a5a762acb9b8516031399c775c2679705446a9bb
Samin-FS <samin@firstsource.tech>
2021-06-22 12:58:30 +0430

    add the series

--- lib/src/add_ons/indicators_ui/ma_env_indicator/ma_env_indicator_config.g.dart
+++ lib/src/add_ons/indicators_ui/ma_env_indicator/ma_env_indicator_config.g.dart
@@ -66,6 +66,7 @@ const _$MovingAverageTypeEnumMap = {
   MovingAverageType.weighted: 'weighted',
   MovingAverageType.hull: 'hull',
   MovingAverageType.zeroLag: 'zeroLag',
+  MovingAverageType.wellesWilder: 'wellesWilder',
 };
 
 const _$ShiftTypeEnumMap = {
