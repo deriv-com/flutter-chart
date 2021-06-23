24e60be6ce79f37a3e191a8c5844d99cb075e78c
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-06-23 12:33:37 +0430

    fix: remove unnecessary comma

--- lib/src/deriv_chart/chart/data_visualization/chart_series/indicators_series/models/indicator_options.dart
+++ lib/src/deriv_chart/chart/data_visualization/chart_series/indicators_series/models/indicator_options.dart
@@ -18,10 +18,7 @@ abstract class IndicatorOptions extends Equatable {
 /// Moving Average indicator options
 class MAOptions extends IndicatorOptions {
   /// Initializes
-  const MAOptions({
-    this.period = 20,
-    this.type = MovingAverageType.simple,
-  });
+  const MAOptions({this.period = 20, this.type = MovingAverageType.simple});
 
   /// The average of this number of past data which will be calculated as MA value.
   final int period;
