db04fb0c60d26ce25eee0e8ed68f3e1f94c1cdfa
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-07-13 19:52:45 +0430

    Paint a path instead of circles

--- lib/src/deriv_chart/chart/worm_chart.dart
+++ lib/src/deriv_chart/chart/worm_chart.dart
@@ -21,16 +21,19 @@ class WormChart extends StatelessWidget {
   final double zoomFactor;
 
   @override
-  Widget build(BuildContext context) => CustomPaint(
-        painter: _WormChartPainter(ticks, zoomFactor),
-      );
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.all(10),
+    child: CustomPaint(
+          painter: _WormChartPainter(ticks, zoomFactor),
+        ),
+  );
 }
 
 class _WormChartPainter extends CustomPainter {
   _WormChartPainter(this.ticks, this.zoomFactor)
       : _paint = Paint()
           ..color = Colors.white
-          ..style = PaintingStyle.fill;
+          ..style = PaintingStyle.stroke;
 
   final List<Tick> ticks;
 
@@ -57,35 +60,45 @@ class _WormChartPainter extends CustomPainter {
     final double min = minMax[0];
     final double max = minMax[1];
 
+    Path? linePath;
+
     for (int i = ticks.length - 1; i >= startIndex; i--) {
       final Tick tick = ticks[i];
-      final double y = _quoteToY(tick.quote, max, min, size.height);
-      final double x = size.width - (ticks.length - i) * ticksDistanceInPx;
+      if (!tick.quote.isNaN) {
+        final double y = _quoteToY(tick.quote, max, min, size.height);
+        final double x = size.width - (ticks.length - i) * ticksDistanceInPx;
 
-      final Offset position = Offset(x, y);
+        if (linePath == null) {
+          linePath = Path()..moveTo(x, y);
+          continue;
+        }
 
-      canvas.drawCircle(position, 1, _paint);
+        final Offset position = Offset(x, y);
 
-      if (tick.quote == max) {
-        canvas.drawCircle(
-          position,
-          2,
-          Paint()
-            ..color = Colors.green
-            ..style = PaintingStyle.fill,
-        );
-      }
+        linePath.lineTo(x, y);
 
-      if (tick.quote == min) {
-        canvas.drawCircle(
-          position,
-          2,
-          Paint()
-            ..color = Colors.red
-            ..style = PaintingStyle.fill,
-        );
+        if (tick.quote == max) {
+          canvas.drawCircle(
+            position,
+            2,
+            Paint()
+              ..color = Colors.green
+              ..style = PaintingStyle.fill,
+          );
+        }
+
+        if (tick.quote == min) {
+          canvas.drawCircle(
+            position,
+            2,
+            Paint()
+              ..color = Colors.red
+              ..style = PaintingStyle.fill,
+          );
+        }
       }
     }
+    canvas.drawPath(linePath!, _paint);
   }
 
   @override
