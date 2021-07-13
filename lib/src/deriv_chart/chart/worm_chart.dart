629ca278969ecd2d112082f1c96920dd51bf911b
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-07-13 19:27:46 +0430

    Add worm Chart

---
+++ lib/src/deriv_chart/chart/worm_chart.dart
@@ -0,0 +1,136 @@
+import 'package:deriv_chart/deriv_chart.dart';
+import 'package:deriv_chart/src/deriv_chart/chart/helpers/functions/conversion.dart';
+import 'package:flutter/material.dart';
+
+///
+class WormChart extends StatelessWidget {
+  /// Initializes
+  const WormChart({
+    required this.ticks,
+    Key? key,
+    this.numberOfVisibleTicks = 10,
+    this.zoomFactor = 0.1,
+  }) : super(key: key);
+
+  /// The ticks list to show.
+  final List<Tick> ticks;
+
+  /// The number of ticks from the [mainSeries.entries] that is going to be shown.
+  final int numberOfVisibleTicks;
+
+  /// Indicates the proportion of the horizontal space that each tick is going to take.
+  ///
+  /// Default is 0.1 which means each tick occupies 10% of the horizontal space,
+  /// and at most 10 ticks will be visible.
+  final double zoomFactor;
+
+  // /// Indicates that the right side of the chart screen is pointing to what index.
+  // ///
+  // /// e.g. 10.2 is somewhere between index 10 and 11.
+  // final double rightBoundIndex;
+
+  @override
+  Widget build(BuildContext context) => CustomPaint(
+        painter: _WormChartPainter(
+          ticks,
+          numberOfVisibleTicks,
+          zoomFactor,
+        ),
+      );
+}
+
+class _WormChartPainter extends CustomPainter {
+  _WormChartPainter(this.ticks, this.numberOfVisibleTicks, this.zoomFactor)
+      : _paint = Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.fill;
+
+  final List<Tick> ticks;
+
+  final int numberOfVisibleTicks;
+
+  final double zoomFactor;
+
+  final Paint _paint;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (ticks.length < 2) {
+      return;
+    }
+
+    final int startIndex = numberOfVisibleTicks >= ticks.length
+        ? 0
+        : ticks.length - numberOfVisibleTicks;
+
+    final double ticksDistanceInPx = zoomFactor * size.width;
+
+    final List<double> minMax = _getMinMax(ticks, startIndex);
+
+    final double min = minMax[0];
+    final double max = minMax[1];
+
+    for (int i = ticks.length - 1; i >= startIndex; i--) {
+      final Tick tick = ticks[i];
+      final double y = _quoteToY(tick.quote, max, min, size.height);
+      final double x = size.width - (ticks.length - i) * ticksDistanceInPx;
+
+      final Offset position = Offset(x, y);
+
+      canvas.drawCircle(position, 1, _paint);
+
+      if (tick.quote == max) {
+        canvas.drawCircle(
+          position,
+          2,
+          Paint()
+            ..color = Colors.green
+            ..style = PaintingStyle.fill,
+        );
+      }
+
+      if (tick.quote == min) {
+        canvas.drawCircle(
+          position,
+          2,
+          Paint()
+            ..color = Colors.red
+            ..style = PaintingStyle.fill,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      true; // TODO(NA): Return the correct value depending on the series.
+}
+
+List<double> _getMinMax(List<Tick> ticks, int startIndex, [int? endIndex]) {
+  final int end = endIndex ?? ticks.length - 1;
+  double min = ticks.last.quote;
+  double max = ticks.last.quote;
+
+  for (int i = startIndex; i < end; i++) {
+    final Tick tick = ticks[i];
+
+    if (tick.quote > max) {
+      max = tick.quote;
+    }
+    if (tick.quote < min) {
+      min = tick.quote;
+    }
+  }
+
+  return <double>[min, max];
+}
+
+double _quoteToY(double quote, double max, double min, double height) =>
+    quoteToCanvasY(
+      quote: quote,
+      topBoundQuote: max,
+      bottomBoundQuote: min,
+      canvasHeight: height,
+      topPadding: 0,
+      bottomPadding: 0,
+    );
