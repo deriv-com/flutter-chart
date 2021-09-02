9335ac472542cab6e4de2004fa28b13fcf940029
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-09-02 11:31:13 +0430

    Add a TODO

--- lib/src/widgets/market_selector/symbol_icon.dart
+++ lib/src/widgets/market_selector/symbol_icon.dart
@@ -33,6 +33,7 @@ class SymbolIcon extends FadeInImage {
             Object error,
             StackTrace? stackTrace,
           ) =>
+              // TODO(NA): Replace with a placeholder which somehow indicates loading icon has failed
               Image.asset(
             _placeHolderPath,
             package: _packageName,
