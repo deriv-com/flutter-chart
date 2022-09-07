8c80c63f5b2cd79dbf37cfde5dff251f7b81c58b
ilya-deriv <ilya@deriv.com>
2022-09-07 12:43:32 +0300

    fix marker circle issue and reformat script

--- lib/src/deriv_chart/chart/data_visualization/markers/marker_icon_painters/marker_icon_painter.dart
+++ lib/src/deriv_chart/chart/data_visualization/markers/marker_icon_painters/marker_icon_painter.dart
@@ -1,17 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/markers/marker.dart';
 import 'package:deriv_chart/src/theme/painting_styles/marker_style.dart';
+import 'package:flutter/material.dart';
 
 /// Foundation class for painting markers on canvas
 abstract class MarkerIconPainter {
   /// Paint circle
-  void paintCircle(Canvas canvas,
-      Offset center,
-      Offset anchor,
-      MarkerDirection direction,
-      MarkerStyle style,) {
+  void paintCircle(
+    Canvas canvas,
+    Offset center,
+    Offset anchor,
+    MarkerDirection direction,
+    MarkerStyle style,
+  ) {
     final Color color =
-    direction == MarkerDirection.up ? style.upColor : style.downColor;
+        direction == MarkerDirection.up ? style.upColor : style.downColor;
 
     canvas
       ..drawLine(
@@ -24,30 +26,31 @@ abstract class MarkerIconPainter {
       ..drawCircle(
         anchor,
         3,
-        Paint()
-          ..color = color,
-      )..drawCircle(
-      anchor,
-      1.5,
-      Paint()
-        ..color = Colors.black,
-    )..drawCircle(
-      center,
-      style.radius,
-      Paint()
-        ..color = color,
-    )..drawCircle(
-      center,
-      style.radius - 2,
-      Paint()
-        ..color = Colors.black.withOpacity(0.32),
-    );
+        Paint()..color = color,
+      )
+      ..drawCircle(
+        anchor,
+        1.5,
+        Paint()..color = Colors.black,
+      )
+      ..drawCircle(
+        center,
+        style.radius,
+        Paint()..color = color,
+      )
+      ..drawCircle(
+        center,
+        style.radius - 2,
+        Paint()..color = Colors.black.withOpacity(0.32),
+      );
   }
 
   /// Paint marker
-  void paintMarker(Canvas canvas,
-      Offset center,
-      Offset anchor,
-      MarkerDirection direction,
-      MarkerStyle style,);
+  void paintMarker(
+    Canvas canvas,
+    Offset center,
+    Offset anchor,
+    MarkerDirection direction,
+    MarkerStyle style,
+  );
 }
--- lib/src/deriv_chart/chart/data_visualization/markers/marker_icon_painters/options_marker_icon_painter.dart
+++ lib/src/deriv_chart/chart/data_visualization/markers/marker_icon_painters/options_marker_icon_painter.dart
@@ -7,11 +7,13 @@ import 'marker_icon_painter.dart';
 /// Icon painter for Options trade type
 class OptionsMarkerIconPainter extends MarkerIconPainter {
   @override
-  void paintMarker(Canvas canvas,
-      Offset center,
-      Offset anchor,
-      MarkerDirection direction,
-      MarkerStyle style,) {
+  void paintMarker(
+    Canvas canvas,
+    Offset center,
+    Offset anchor,
+    MarkerDirection direction,
+    MarkerStyle style,
+  ) {
     paintCircle(canvas, center, anchor, direction, style);
 
     final double dir = direction == MarkerDirection.up ? 1 : -1;
@@ -25,7 +27,7 @@ class OptionsMarkerIconPainter extends MarkerIconPainter {
         center.dy - (const Size(20, 20).height / 4) * dir,
       )
 
-    // 16x16 is the original svg size.
+      // 16x16 is the original svg size.
       ..scale(
         const Size(20, 20).width / 16,
         const Size(20, 20).height / 16 * dir,
@@ -34,22 +36,30 @@ class OptionsMarkerIconPainter extends MarkerIconPainter {
     // This path was generated with http://demo.qunee.com/svg2canvas/.
     path
       ..moveTo(7, 0)
-      ..lineTo(4.5, 0)..lineTo(4.5, 0.005)
+      ..lineTo(4.5, 0)
+      ..lineTo(4.5, 0.005)
       ..cubicTo(4.5, 0.554523, 4.94548, 1, 5.495, 1)
-      ..lineTo(6.295, 1)..lineTo(2, 5.293)..lineTo(2, 6.707)..lineTo(
-        7, 1.707)..lineTo(7, 2.507)
+      ..lineTo(6.295, 1)
+      ..lineTo(2, 5.293)
+      ..lineTo(2, 6.707)
+      ..lineTo(7, 1.707)
+      ..lineTo(7, 2.507)
       ..cubicTo(7.0011, 3.05574, 7.44626, 3.5, 7.995, 3.5)
-      ..lineTo(8, 3.5)..lineTo(8, 1)
+      ..lineTo(8, 3.5)
+      ..lineTo(8, 1)
       ..cubicTo(8, 0.447715, 7.55228, 0, 7, 0)
       ..close()
       ..moveTo(0.707, 8)
-      ..lineTo(0, 8)..lineTo(0, 7)..lineTo(1, 7)..lineTo(1.707, 7)..lineTo(
-        1, 7.707)..lineTo(0.707, 8)
+      ..lineTo(0, 8)
+      ..lineTo(0, 7)
+      ..lineTo(1, 7)
+      ..lineTo(1.707, 7)
+      ..lineTo(1, 7.707)
+      ..lineTo(0.707, 8)
       ..close();
 
     canvas
-      ..drawPath(path, Paint()
-        ..color = Colors.white)
+      ..drawPath(path, Paint()..color = Colors.white)
       ..restore();
   }
 }
