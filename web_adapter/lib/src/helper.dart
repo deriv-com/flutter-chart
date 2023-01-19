1178b6c2840b0c84dc979ca3e5917b8da7ce6795
balakrishna-binary <bala.krishna@regentmarkets.com>
2023-01-19 14:30:36 +0800

    chore: remove web_adapter

--- web_adapter/lib/src/helper.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Gets [Color] instance from hex code
-Color getColorFromHex(String hexColor) =>
-    Color(int.parse(hexColor.replaceAll('#', '0xff')));
