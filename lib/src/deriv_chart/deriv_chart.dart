b8421a919c74f7849da4d38d9dba8e9cccdf68a1
Bahar <bahar@regentmarkets.com>
2023-06-09 11:07:24 +0800

    bahar/82785/Make drawings draggable (#224)
    * feat: make_drawings_draggable
    
    * line_changes_added_before_cleanup
    
    * cleanup
    
    * extract_drawing_tool_chart
    
    * fixed_crosshair_issue
    
    * fix_long_press_issue
    
    * make_drawing_class_abstract
    
    * fix_review_issues
    
    * added_enum_for_darwing_parts
    
    * fix_review_comments
    
    * fix_review_pointed_out_issues
    
    * fix_comments
    
    * add initial value for draggable edge point and remove extra inputs
    
    * add vector.zero
    
    * drawing_selection_added
    
    * review_comments
    
    * handle unfinished drawings
    
    * fix_review_comments
    
    * fix_json_autogenerated_files_issue

--- lib/src/deriv_chart/deriv_chart.dart
+++ lib/src/deriv_chart/deriv_chart.dart
@@ -184,6 +184,7 @@ class _DerivChartState extends State<DerivChart> {
                 drawings: _drawings,
                 onAddDrawing: _onAddDrawing,
                 selectedDrawingTool: _selectedDrawingTool,
+                clearDrawingToolSelection: _clearDrawingToolSelection,
                 markerSeries: widget.markerSeries,
                 theme: widget.theme,
                 onCrosshairAppeared: widget.onCrosshairAppeared,
@@ -217,6 +218,15 @@ class _DerivChartState extends State<DerivChart> {
                 child: IconButton(
                   icon: const Icon(Icons.drive_file_rename_outline_outlined),
                   onPressed: () {
+                    /// Remove unfinished drawings before openning the dialog.
+                    /// For the scenario where the user adds part of a drawing
+                    /// and then opens the dialog.
+                    setState(() {
+                      _drawings.removeWhere(
+                          (DrawingData data) => !data.isDrawingFinished);
+                      _selectedDrawingTool = null;
+                    });
+
                     showDialog<void>(
                       context: context,
                       builder: (
@@ -270,16 +280,32 @@ class _DerivChartState extends State<DerivChart> {
         _drawings.add(DrawingData(
           id: drawingId,
           config: _selectedDrawingTool!,
-          drawings: addedDrawing.values.first,
+          drawingParts: addedDrawing.values.first,
+          isDrawingFinished: isDrawingFinished,
         ));
       } else {
-        existingDrawing.updateDrawingList(addedDrawing.values.first);
+        existingDrawing
+          ..updateDrawingPartList(addedDrawing.values.first)
+          ..isSelected = true
+          ..isDrawingFinished = isDrawingFinished;
       }
 
       if (isDrawingFinished) {
         _drawingToolsRepo.add(_selectedDrawingTool!);
         _selectedDrawingTool = null;
       }
+
+      if (_drawings.length > 1) {
+        _drawings.removeWhere((DrawingData data) =>
+            data.id != drawingId && !data.isDrawingFinished);
+      }
+    });
+  }
+
+  /// Clean the drawing tool selection.
+  void _clearDrawingToolSelection() {
+    setState(() {
+      _selectedDrawingTool = null;
     });
   }
 }
