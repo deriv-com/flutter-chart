50b9fd003b903133ecea6835b917399de9e4d5a5
ramin-deriv <ramin.vakili@deriv.com>
2023-09-29 11:07:30 +0800

    improve DraggableEdgePointExtension doc

--- lib/src/deriv_chart/chart/data_visualization/drawing_tools/data_model/extensions.dart
+++ lib/src/deriv_chart/chart/data_visualization/drawing_tools/data_model/extensions.dart
@@ -14,7 +14,14 @@ import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/drawing_too
 /// we can improve this value.
 ///
 /// Currently as for a safe number we consider the half of screen width for a
-/// [DraggableEdgePoint] to be considered as off screen.
+/// [DraggableEdgePoint] to be considered as off screen. This will ensure on all
+/// granularities the [DraggableEdgePoint] is fully out of the view port.
+///
+///       view port      half of screen outside
+///    ------^-------- ---^---
+///   |               |   *  |    -> edge point is NOT considered as off screen.
+///   |               |      |
+///   |               |      |  * -> edge point is considered as off screen.
 double _edgePointOffScreenSafeDistance(int leftEpoch, int rightEpoch) =>
     (rightEpoch - leftEpoch) / 2;
 
