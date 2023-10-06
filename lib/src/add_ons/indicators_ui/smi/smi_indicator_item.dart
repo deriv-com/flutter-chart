12c926fbf0600c55ec5000170d1f8cdbff75ab2a
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2023-10-06 10:50:27 +0800

    Feat drawingtool integration (#247)
    - drawing tools integration

--- lib/src/add_ons/indicators_ui/smi/smi_indicator_item.dart
+++ lib/src/add_ons/indicators_ui/smi/smi_indicator_item.dart
@@ -4,7 +4,6 @@ import 'package:deriv_chart/src/add_ons/indicators_ui/widgets/dropdown_menu.dart
     as deriv_dropdown;
 import 'package:deriv_chart/src/add_ons/indicators_ui/widgets/field_widget.dart';
 import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/chart_series/indicators_series/ma_series.dart';
-
 import 'package:flutter/material.dart' hide DropdownMenu;
 
 import '../callbacks.dart';
