bfb851565ba66385b40521a0187b53d66336981e
Henry Hein <henry@regentmarkets.com>
2024-03-14 16:52:24 +0800

    fix: make import more specific rather than importing the whole deriv_chart folder

--- lib/src/add_ons/indicators_ui/smi/smi_indicator_item.dart
+++ lib/src/add_ons/indicators_ui/smi/smi_indicator_item.dart
@@ -1,4 +1,4 @@
-import 'package:deriv_chart/deriv_chart.dart';
+import 'package:deriv_chart/src/add_ons/indicators_ui/oscillator_lines/oscillator_lines_config.dart';
 import 'package:deriv_chart/src/add_ons/indicators_ui/indicator_config.dart';
 import 'package:deriv_chart/src/add_ons/indicators_ui/widgets/dropdown_menu.dart'
     as deriv_dropdown;
