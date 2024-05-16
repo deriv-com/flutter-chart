f38e35955b8e3e4e7d2208d2e95e6c6a8265affd
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2024-05-16 11:48:20 +0800

    review fix

--- lib/src/deriv_chart/chart/y_axis/y_axis_config.dart
+++ lib/src/deriv_chart/chart/y_axis/y_axis_config.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-///Singleton class to manage configuration and operations related to the Y-axis 
+///Singleton class to manage configuration and operations related to the Y-axis
 ///in custom painting
 class YAxisConfig {
   //Private constructor to prevent external instantiation.
@@ -8,21 +8,21 @@ class YAxisConfig {
 
   static final YAxisConfig _instance = YAxisConfig._();
 
-  ///  // The single instance of `YAxisConfig`.
+  /// The single instance of `YAxisConfig`.
   static YAxisConfig get instance => _instance;
 
-  ///Cached width of the label on the Y-axis. This is nullable to handle cases 
+  ///Cached width of the label on the Y-axis. This is nullable to handle cases
   ///where it might not be set yet.
   double? cachedLabelWidth;
 
-  ///Sets and caches the label width for the Y-axis. Returns the set width for 
+  ///Sets and caches the label width for the Y-axis. Returns the set width for
   ///potential immediate use.
   double setLabelWidth(double width) {
     cachedLabelWidth = width;
     return cachedLabelWidth!;
   }
 
-  ///Executes painting logic within a clipped area of the canvas to prevent 
+  ///Executes painting logic within a clipped area of the canvas to prevent
   ///drawing over the Y-axis labels.
   void yAxisClipping(Canvas canvas, Size size, VoidCallback paintingLogic) {
     final Rect clipRect =
