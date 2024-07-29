6d8c417aa57348e3a0c39ecc5bbad925a4e7fd7b
ramin-deriv <ramin.vakili@deriv.com>
2024-07-29 17:07:00 +0800

    show frame for bottom indicators charts

--- lib/src/deriv_chart/chart/mobile_chart_frame_dividers.dart
+++ lib/src/deriv_chart/chart/mobile_chart_frame_dividers.dart
@@ -6,8 +6,9 @@ class MobileChartFrameDividers extends StatelessWidget {
   const MobileChartFrameDividers({
     required this.color,
     required this.rightPadding,
-    super.key,
+    this.sides = const ChartFrameSides.all(value: true),
     this.thickness = 1,
+    super.key,
   });
 
   /// The padding on the right side of the chart frame.
@@ -19,14 +20,17 @@ class MobileChartFrameDividers extends StatelessWidget {
   /// The thickness of the dividers.
   final double thickness;
 
+  /// The sides of the chart frame.
+  final ChartFrameSides sides;
+
   @override
   Widget build(BuildContext context) => CustomPaint(
-        painter: _ChartFramePainter(rightPadding, color, thickness),
+        painter: _ChartFramePainter(rightPadding, color, thickness, sides),
       );
 }
 
 class _ChartFramePainter extends CustomPainter {
-  _ChartFramePainter(this.rightPadding, this.color, this.thickness)
+  _ChartFramePainter(this.rightPadding, this.color, this.thickness, this.sides)
       : _paint = Paint()
           ..color = color
           ..strokeWidth = thickness;
@@ -34,29 +38,62 @@ class _ChartFramePainter extends CustomPainter {
   final double rightPadding;
   final Color color;
   final double thickness;
+  final ChartFrameSides sides;
 
   final Paint _paint;
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas
-      ..drawLine(
+    if (sides.top) {
+      canvas.drawLine(
         Offset.zero,
         Offset(size.width - rightPadding, 0),
         _paint,
-      )
-      ..drawLine(
+      );
+    }
+
+    if (sides.right) {
+      canvas.drawLine(
         Offset(size.width - rightPadding, 0),
         Offset(size.width - rightPadding, size.height),
         _paint,
-      )
-      ..drawLine(
+      );
+    }
+
+    if (sides.bottom) {
+      canvas.drawLine(
         Offset(0, size.height),
         Offset(size.width - rightPadding, size.height),
         _paint,
       );
+    }
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+/// A class to define the sides of the chart frame.
+class ChartFrameSides {
+  /// Initializes the sides of the chart frame.
+  const ChartFrameSides({
+    this.top = false,
+    this.right = false,
+    this.bottom = false,
+  });
+
+  /// Initializes all sides of the chart frame as true.
+  const ChartFrameSides.all({bool value = false})
+      : top = value,
+        right = value,
+        bottom = value;
+
+  /// Top side is visible.
+  final bool top;
+
+  /// Right side is visible.
+  final bool right;
+
+  /// Bottom side is visible.
+  final bool bottom;
 }
