55ef43d4a46ab0a8c36d8eb03cc9f27f795f08f7
ramin-deriv <ramin.vakili@deriv.com>
2024-07-31 13:58:08 +0800

    consider safe distance for Annotations shouldRepaint functionality

--- lib/src/deriv_chart/chart/data_visualization/extensions/extensions.dart
+++ lib/src/deriv_chart/chart/data_visualization/extensions/extensions.dart
@@ -22,7 +22,7 @@ import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/drawing_too
 ///   |               |   *  |    -> edge point is NOT considered as off screen.
 ///   |               |      |
 ///   |               |      |  * -> edge point is considered as off screen.
-double _edgePointOffScreenSafeDistance(int leftEpoch, int rightEpoch) =>
+double edgePointOffScreenSafeDistance(int leftEpoch, int rightEpoch) =>
     (rightEpoch - leftEpoch) / 2;
 
 /// An extension on DraggableEdgePoint class that adds some helper methods.
@@ -33,8 +33,7 @@ extension DraggableEdgePointExtension on DraggableEdgePoint {
   /// returns true if the edge point is on the view port range.
   bool isInViewPortRange(int leftEpoch, int rightEpoch) =>
       draggedEdgePoint.epoch >=
-          (leftEpoch -
-              _edgePointOffScreenSafeDistance(leftEpoch, rightEpoch)) &&
+          (leftEpoch - edgePointOffScreenSafeDistance(leftEpoch, rightEpoch)) &&
       draggedEdgePoint.epoch <=
-          (rightEpoch + _edgePointOffScreenSafeDistance(leftEpoch, rightEpoch));
+          (rightEpoch + edgePointOffScreenSafeDistance(leftEpoch, rightEpoch));
 }
