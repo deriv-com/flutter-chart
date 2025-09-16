433c8dd7e0c15bbdb849ed0e228ba1408607d54f
Jim Daniels Wasswa <jim@regentmarkets.com>
2025-09-16 12:56:37 +0800

    style: remove unnecessary blank line

--- lib/src/deriv_chart/interactive_layer/drawing_tool_gesture_recognizer.dart
+++ lib/src/deriv_chart/interactive_layer/drawing_tool_gesture_recognizer.dart
@@ -93,7 +93,6 @@ class DrawingToolGestureRecognizer extends OneSequenceGestureRecognizer {
   /// Whether the pointer has moved significantly (indicating drag, not long press).
   bool _hasMovedSignificantly = false;
 
-
   @override
   void addPointer(PointerDownEvent event) {
     _globalStartPosition = event.position;
